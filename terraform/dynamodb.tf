4eb99bf0a00d6c8499abb1a9dd818120db87952e
Max Neuvians <max.neuvians@cds-snc.ca>
2022-03-23 22:16:10 +0000

    fix: PPR attribute

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,7 +1,6 @@
 resource "aws_dynamodb_table" "webhooks_table" {
   name           = "webhooks"
   hash_key       = "id"
-  billing_mode   = "PAY_PER_REQUEST"
   read_capacity  = 1
   write_capacity = 1
 
