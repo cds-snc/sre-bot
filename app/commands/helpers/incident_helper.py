f836aac1499495e246b9d632e78885a9bb6d3b73
Max Neuvians <max.neuvians@cds-snc.ca>
2022-04-04 19:32:54 +0000

    feat: adds metadata to folder

--- app/commands/helpers/incident_helper.py
+++ app/commands/helpers/incident_helper.py
@@ -6,7 +6,7 @@ help_text = """
 \n `/sre incident list-folders` - list all folders in the incident drive"""
 
 
-def handle_incident_command(args, client, body, respond):
+def handle_incident_command(args, client, body, respond, ack):
 
     if len(args) == 0:
         respond(help_text)
@@ -20,9 +20,199 @@ def handle_incident_command(args, client, body, respond):
         case "help":
             respond(help_text)
         case "list-folders":
-            names = list(n["name"] for n in google_drive.list_folders())
-            respond(", ".join(names))
+            list_folders(client, body, ack)
         case _:
             respond(
                 f"Unknown command: {action}. Type `/sre incident help` to see a list of commands."
             )
+
+
+def add_folder_metadata(client, body, ack):
+    ack()
+    folder_id = body["actions"][0]["value"]
+    blocks = {
+        "type": "modal",
+        "callback_id": "add_metadata_view",
+        "title": {"type": "plain_text", "text": "SRE - Add metadata"},
+        "submit": {"type": "plain_text", "text": "Save metadata"},
+        "close": {"type": "plain_text", "text": "Cancel"},
+        "private_metadata": folder_id,
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "*Add metadata*",
+                },
+            },
+            {
+                "type": "input",
+                "block_id": "key",
+                "element": {
+                    "type": "plain_text_input",
+                    "action_id": "key",
+                    "placeholder": {"type": "plain_text", "text": "Key"},
+                },
+                "label": {
+                    "type": "plain_text",
+                    "text": "Key",
+                },
+            },
+            {
+                "type": "input",
+                "block_id": "value",
+                "element": {
+                    "type": "plain_text_input",
+                    "action_id": "value",
+                    "placeholder": {"type": "plain_text", "text": "Value"},
+                },
+                "label": {
+                    "type": "plain_text",
+                    "text": "Value",
+                },
+            },
+        ],
+    }
+    client.views_update(
+        view_id=body["view"]["id"],
+        view=blocks,
+    )
+
+
+def delete_folder_metadata(client, body, ack):
+    ack()
+    folder_id = body["view"]["private_metadata"]
+    key = body["actions"][0]["value"]
+    google_drive.delete_metadata(folder_id, key)
+    body["actions"] = [{"value": folder_id}]
+    view_folder_metadata(client, body, ack)
+
+
+def list_folders(client, body, ack):
+    ack()
+    folders = google_drive.list_folders()
+    folders.sort(key=lambda x: x["name"])
+    blocks = {
+        "type": "modal",
+        "callback_id": "list_folders_view",
+        "title": {"type": "plain_text", "text": "SRE - Listing folders"},
+        "close": {"type": "plain_text", "text": "Close"},
+        "blocks": [
+            item for sublist in list(map(folder_item, folders)) for item in sublist
+        ],
+    }
+    client.views_open(trigger_id=body["trigger_id"], view=blocks)
+
+
+def save_metadata(client, body, ack, view):
+    ack()
+    folder_id = view["private_metadata"]
+    key = view["state"]["values"]["key"]["key"]["value"]
+    value = view["state"]["values"]["value"]["value"]["value"]
+    google_drive.add_metadata(folder_id, key, value)
+    body["actions"] = [{"value": folder_id}]
+    del body["view"]
+    view_folder_metadata(client, body, ack)
+
+
+def view_folder_metadata(client, body, ack):
+    ack()
+    folder_id = body["actions"][0]["value"]
+    folder = google_drive.list_metadata(folder_id)
+    blocks = {
+        "type": "modal",
+        "callback_id": "view_folder_metadata_modal",
+        "title": {"type": "plain_text", "text": "SRE - Showing metadata"},
+        "submit": {"type": "plain_text", "text": "Return to folders"},
+        "private_metadata": folder_id,
+        "blocks": (
+            [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "plain_text",
+                        "text": folder["name"],
+                    },
+                    "accessory": {
+                        "type": "button",
+                        "text": {"type": "plain_text", "text": "Add metadata"},
+                        "value": folder_id,
+                        "action_id": "add_folder_metadata",
+                    },
+                },
+                {"type": "divider"},
+            ]
+            + metadata_items(folder)
+        ),
+    }
+    if "view" in body:
+        client.views_update(
+            view_id=body["view"]["id"],
+            view=blocks,
+        )
+    else:
+        client.views_open(trigger_id=body["trigger_id"], view=blocks)
+
+
+def folder_item(folder):
+    return [
+        {
+            "type": "section",
+            "text": {"type": "mrkdwn", "text": f"*{folder['name']}*"},
+            "accessory": {
+                "type": "button",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Manage metadata",
+                    "emoji": True,
+                },
+                "value": f"{folder['id']}",
+                "action_id": "view_folder_metadata",
+            },
+        },
+        {
+            "type": "context",
+            "elements": [
+                {
+                    "type": "mrkdwn",
+                    "text": f"<https://drive.google.com/drive/u/0/folders/{folder['id']}|View in Google Drive>",
+                }
+            ],
+        },
+        {"type": "divider"},
+    ]
+
+
+def metadata_items(folder):
+    if "appProperties" not in folder or len(folder["appProperties"]) == 0:
+        return [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "*No metadata found. Click the button above to add metadata.*",
+                },
+            },
+        ]
+    else:
+        return [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": f"*{key}*\n{value}",
+                },
+                "accessory": {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Delete metadata",
+                        "emoji": True,
+                    },
+                    "value": key,
+                    "style": "danger",
+                    "action_id": "delete_folder_metadata",
+                },
+            }
+            for key, value in folder["appProperties"].items()
+        ]
--- app/tests/commands/helpers/test_incident_helper.py
+++ app/tests/commands/helpers/test_incident_helper.py
@@ -1,12 +1,13 @@
 from commands.helpers import incident_helper
 
 
-from unittest.mock import MagicMock, patch
+from unittest.mock import ANY, MagicMock, patch
 
 
 def test_handle_incident_command_with_empty_args():
     respond = MagicMock()
-    incident_helper.handle_incident_command([], MagicMock(), MagicMock(), respond)
+    ack = MagicMock()
+    incident_helper.handle_incident_command([], MagicMock(), MagicMock(), respond, ack)
     respond.assert_called_once_with(incident_helper.help_text)
 
 
@@ -14,31 +15,213 @@ def test_handle_incident_command_with_empty_args():
 def test_handle_incident_command_with_create_command(create_folder_mock):
     create_folder_mock.return_value = "folder created"
     respond = MagicMock()
+    ack = MagicMock()
     incident_helper.handle_incident_command(
-        ["create-folder", "foo", "bar"], MagicMock(), MagicMock(), respond
+        ["create-folder", "foo", "bar"], MagicMock(), MagicMock(), respond, ack
     )
     respond.assert_called_once_with("folder created")
 
 
 def test_handle_incident_command_with_help():
     respond = MagicMock()
-    incident_helper.handle_incident_command(["help"], MagicMock(), MagicMock(), respond)
+    ack = MagicMock()
+    incident_helper.handle_incident_command(
+        ["help"], MagicMock(), MagicMock(), respond, ack
+    )
     respond.assert_called_once_with(incident_helper.help_text)
 
 
-@patch("commands.helpers.incident_helper.google_drive.list_folders")
+@patch("commands.helpers.incident_helper.list_folders")
 def test_handle_incident_command_with_list_folders(list_folders_mock):
+    client = MagicMock()
+    body = MagicMock()
     respond = MagicMock()
-    list_folders_mock.return_value = [{"name": "foo"}, {"name": "bar"}]
+    ack = MagicMock()
     incident_helper.handle_incident_command(
-        ["list-folders"], MagicMock(), MagicMock(), respond
+        ["list-folders"], client, body, respond, ack
     )
-    respond.assert_called_once_with("foo, bar")
+    list_folders_mock.assert_called_once_with(client, body, ack)
 
 
 def test_handle_incident_command_with_unknown_command():
     respond = MagicMock()
-    incident_helper.handle_incident_command(["foo"], MagicMock(), MagicMock(), respond)
+    ack = MagicMock()
+    incident_helper.handle_incident_command(
+        ["foo"], MagicMock(), MagicMock(), respond, ack
+    )
     respond.assert_called_once_with(
         "Unknown command: foo. Type `/sre incident help` to see a list of commands."
     )
+
+
+def test_add_folder_metadata():
+    client = MagicMock()
+    body = {"actions": [{"value": "foo"}], "view": {"id": "bar"}}
+    ack = MagicMock()
+    incident_helper.add_folder_metadata(client, body, ack)
+    ack.assert_called_once()
+    client.views_update.assert_called_once_with(view_id="bar", view=ANY)
+
+
+@patch("commands.helpers.incident_helper.google_drive.delete_metadata")
+@patch("commands.helpers.incident_helper.view_folder_metadata")
+def test_delete_folder_metadata(view_folder_metadata_mock, delete_metadata_mock):
+    client = MagicMock()
+    body = {"actions": [{"value": "foo"}], "view": {"private_metadata": "bar"}}
+    ack = MagicMock()
+    incident_helper.delete_folder_metadata(client, body, ack)
+    ack.assert_called_once()
+    delete_metadata_mock.assert_called_once_with("bar", "foo")
+    view_folder_metadata_mock.assert_called_once_with(
+        client,
+        {"actions": [{"value": "bar"}], "view": {"private_metadata": "bar"}},
+        ack,
+    )
+
+
+@patch("commands.helpers.incident_helper.google_drive.list_folders")
+@patch("commands.helpers.incident_helper.folder_item")
+def test_list_folders(folder_item_mock, list_folders_mock):
+    client = MagicMock()
+    body = {"trigger_id": "foo"}
+    ack = MagicMock()
+    list_folders_mock.return_value = [{"id": "foo", "name": "bar"}]
+    folder_item_mock.return_value = [["folder item"]]
+    incident_helper.list_folders(client, body, ack)
+    list_folders_mock.assert_called_once()
+    folder_item_mock.assert_called_once_with({"id": "foo", "name": "bar"})
+    ack.assert_called_once()
+    client.views_open.assert_called_once_with(trigger_id="foo", view=ANY)
+
+
+@patch("commands.helpers.incident_helper.google_drive.add_metadata")
+@patch("commands.helpers.incident_helper.view_folder_metadata")
+def test_save_metadata(view_folder_metadata_mock, add_metadata_mock):
+    client = MagicMock()
+    body = {"actions": [{"value": "foo"}], "view": {"private_metadata": "bar"}}
+    view = {
+        "state": {
+            "values": {
+                "key": {"key": {"value": "key"}},
+                "value": {"value": {"value": "value"}},
+            }
+        },
+        "private_metadata": "bar",
+    }
+    ack = MagicMock()
+    incident_helper.save_metadata(client, body, ack, view)
+    ack.assert_called_once()
+    add_metadata_mock.assert_called_once_with("bar", "key", "value")
+    view_folder_metadata_mock.assert_called_once_with(
+        client,
+        {"actions": [{"value": "bar"}]},
+        ack,
+    )
+
+
+@patch("commands.helpers.incident_helper.google_drive.list_metadata")
+@patch("commands.helpers.incident_helper.metadata_items")
+def test_view_folder_metadata_open(metadata_items_mock, list_metadata_mock):
+    client = MagicMock()
+    body = {"actions": [{"value": "foo"}], "trigger_id": "trigger_id"}
+    ack = MagicMock()
+    list_metadata_mock.return_value = {
+        "name": "folder",
+        "appProperties": [{"key": "key", "value": "value"}],
+    }
+
+    metadata_items_mock.return_value = [["metadata item"]]
+    incident_helper.view_folder_metadata(client, body, ack)
+    ack.assert_called_once()
+    list_metadata_mock.assert_called_once_with("foo")
+    metadata_items_mock.assert_called_once_with(
+        {"name": "folder", "appProperties": [{"key": "key", "value": "value"}]}
+    )
+    client.views_open(trigger_id="trigger_id", view=ANY)
+
+
+@patch("commands.helpers.incident_helper.google_drive.list_metadata")
+@patch("commands.helpers.incident_helper.metadata_items")
+def test_view_folder_metadata_update(metadata_items_mock, list_metadata_mock):
+    client = MagicMock()
+    body = {"actions": [{"value": "foo"}], "view": {"id": "view_id"}}
+    ack = MagicMock()
+    list_metadata_mock.return_value = {
+        "name": "folder",
+        "appProperties": [{"key": "key", "value": "value"}],
+    }
+
+    metadata_items_mock.return_value = [["metadata item"]]
+    incident_helper.view_folder_metadata(client, body, ack)
+    ack.assert_called_once()
+    list_metadata_mock.assert_called_once_with("foo")
+    metadata_items_mock.assert_called_once_with(
+        {"name": "folder", "appProperties": [{"key": "key", "value": "value"}]}
+    )
+    client.views_update(view_id="view_id", view=ANY)
+
+
+def test_folder_item():
+    assert incident_helper.folder_item({"id": "foo", "name": "bar"}) == [
+        {
+            "accessory": {
+                "action_id": "view_folder_metadata",
+                "text": {
+                    "emoji": True,
+                    "text": "Manage metadata",
+                    "type": "plain_text",
+                },
+                "type": "button",
+                "value": "foo",
+            },
+            "text": {"text": "*bar*", "type": "mrkdwn"},
+            "type": "section",
+        },
+        {
+            "elements": [
+                {
+                    "text": "<https://drive.google.com/drive/u/0/folders/foo|View in Google Drive>",
+                    "type": "mrkdwn",
+                }
+            ],
+            "type": "context",
+        },
+        {"type": "divider"},
+    ]
+
+
+def test_metadata_items_empty():
+    empty = [
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "*No metadata found. Click the button above to add metadata.*",
+            },
+        },
+    ]
+    assert incident_helper.metadata_items({}) == empty
+    assert incident_helper.metadata_items({"appProperties": []}) == empty
+
+
+def test_metadata_items():
+    assert incident_helper.metadata_items({"appProperties": {"key": "value"}}) == [
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": f"*key*\nvalue",
+            },
+            "accessory": {
+                "type": "button",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Delete metadata",
+                    "emoji": True,
+                },
+                "value": "key",
+                "style": "danger",
+                "action_id": "delete_folder_metadata",
+            },
+        },
+    ]
