af37e336b785f79a9efae943f1deae5e3c947b09
Max Neuvians <max.neuvians@cds-snc.ca>
2022-04-05 20:28:09 +0000

    fix: IAM permission

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -21,6 +21,7 @@ data "aws_iam_policy_document" "sre-bot_secrets_manager" {
       aws_secretsmanager_secret.slack_token.arn,
       aws_secretsmanager_secret.app_token.arn,
       aws_secretsmanager_secret.pickle_string.arn,
+      aws_secretsmanager_secret.opsgenie_key.arn,
     ]
   }
 
