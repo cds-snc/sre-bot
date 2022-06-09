6f52c26c9a4e1e6294db189ac02571d80078813b
Max Neuvians <max.neuvians@cds-snc.ca>
2022-06-09 09:00:13 -0400

    fix: table def

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -12,12 +12,18 @@ resource "aws_dynamodb_table" "webhooks_table" {
 
 resource "aws_dynamodb_table" "aws_access_requests_table" {
   name           = "aws_access_requests"
-  hash_key       = "id"
+  hash_key       = "account_id"
+  range_key      = "created_at"
   read_capacity  = 1
   write_capacity = 1
 
   attribute {
-    name = "id"
+    name = "account_id"
     type = "S"
+  }
+
+  attribute {
+    name = "created_at"
+    type = "N"
   }
 }
