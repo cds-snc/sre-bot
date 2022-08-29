5c0086008540caf018a50df0d7c9a0c94ae2ede2
Guillaume Charest <guillaume.charest@cds-snc.ca>
2022-08-29 13:56:49 +0000

    feat: Reformat text layout

--- app/commands/sre.py
+++ app/commands/sre.py
@@ -5,11 +5,21 @@ from commands import utils
 from commands.helpers import geolocate_helper, incident_helper, webhook_helper
 
 help_text = """
-\n `/sre help` - show this help text | montre le texte d'aide
-\n `/sre geolocate <ip>` - geolocate an IP address | géolocaliser une adresse IP
-\n `/sre incident` - lists incident commands | lister les commandes d'incidents
-\n `/sre webhooks` - lists webhook commands | lister les commandes de liens de rappel HTTP
-\n `/sre version` - show the version of the SRE Bot | montre la version du bot SRE"""
+\n `/sre help`
+\n      - show this help text
+\n      - montre le texte d'aide
+\n `/sre geolocate <ip>`
+\n      - geolocate an IP address
+\n      - géolocaliser une adresse IP
+\n `/sre incident`
+\n      - lists incident commands
+\n      - lister les commandes d'incidents
+\n `/sre webhooks`
+\n      - lists webhook commands
+\n      - lister les commandes de liens de rappel HTTP
+\n `/sre version`
+\n      - show the version of the SRE Bot
+\n      - montre la version du bot SRE"""
 
 
 def sre_command(ack, command, logger, respond, client, body):
@@ -17,7 +27,7 @@ def sre_command(ack, command, logger, respond, client, body):
     logger.info("SRE command received: %s", command["text"])
 
     if command["text"] == "":
-        respond("Type `/sre help` to see a list of commands.")
+        respond("Type `/sre help` to see a list of commands.\nTapez `/sre help` pour voir une liste de commandes")
         return
 
     action, *args = utils.parse_command(command["text"])
@@ -26,8 +36,10 @@ def sre_command(ack, command, logger, respond, client, body):
             respond(help_text)
         case "geolocate":
             if len(args) == 0:
-                respond("Please provide an IP address. | "
-                "SVP fournir une adresse IP")
+                respond(
+                    "Please provide an IP address.\n"
+                    "SVP fournir une adresse IP"
+                )
                 return
             geolocate_helper.geolocate(args, respond)
         case "incident":
