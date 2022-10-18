8526bb4c1df95caa857971262eac67ff4285962f
Max Neuvians <max.neuvians@cds-snc.ca>
2022-10-18 16:51:57 +0000

    test: sensitive redaction;

---
+++ terraform/test.tf
@@ -0,0 +1,24 @@
+variable "my_secret" {
+  description = "This is a secret"
+  type        = string
+  sensitive   = true
+}
+
+
+data "aws_iam_policy_document" "test_role" {
+  statement {
+    sid     = "AssumeRole"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type = "AWS"
+      identifiers = [
+        var.secret
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "sre_bot" {
+  name               = "test_role-${var.secret}"
+  assume_role_policy = data.aws_iam_policy_document.test_role.json
+}
