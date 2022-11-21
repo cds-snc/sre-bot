5f2549420de26b15d148bf44f4202f56fd6d3a92
Max Neuvians <max.neuvians@cds-snc.ca>
2022-11-21 18:26:06 +0000

    feat: add new global table for SRE bot

--- .devcontainer/dynamodb-create.sh
+++ .devcontainer/dynamodb-create.sh
@@ -12,3 +12,11 @@ aws dynamodb create-table \
    --key-schema AttributeName=account_id,KeyType=HASH AttributeName=created_at,KeyType=RANGE \
    --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 \
    --endpoint-url http://dynamodb-local:8000
+
+
+aws dynamodb create-table \
+   --table-name sre_bot_data \
+   --attribute-definitions AttributeName=PK,AttributeType=S AttributeName=SK,AttributeType=S \
+   --key-schema AttributeName=PK,KeyType=HASH AttributeName=SK,KeyType=RANGE \
+   --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 \
+   --endpoint-url http://dynamodb-local:8000
