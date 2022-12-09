5757ec01449d45636a298b267a4f982d04cfeeba
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2022-12-09 16:13:52 +0000

    fix: add missing main tests

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -15,9 +15,20 @@ def test_main_invokes_socket_handler(
 
     mock_app.assert_called_once_with(token=os.environ.get("SLACK_TOKEN"))
 
+    mock_app.return_value.command.assert_any_call("/atip")
+    mock_app.return_value.command.assert_any_call("/aiprp")
+    mock_app.return_value.view.assert_any_call("atip_view")
+    mock_app.return_value.action.assert_any_call("ati_search_width")
+    mock_app.return_value.action.assert_any_call("atip_change_locale")
+
+    mock_app.return_value.command.assert_any_call("/aws")
+    mock_app.return_value.view.assert_any_call("aws_access_view")
+    mock_app.return_value.view.assert_any_call("aws_health_view")
+
     mock_app.return_value.command.assert_any_call("/incident")
     mock_app.return_value.view.assert_any_call("incident_view")
     mock_app.return_value.action.assert_any_call("handle_incident_action_buttons")
+    mock_app.return_value.action.assert_any_call("incident_change_locale")
 
     mock_app.return_value.action.assert_any_call("add_folder_metadata")
     mock_app.return_value.action.assert_any_call("view_folder_metadata")
