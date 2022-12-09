e593c09acb1f32d93b991b96f51d414ce6d3f84e
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2022-12-09 01:31:58 +0000

    feat: add incident_modal_view function

--- app/commands/incident.py
+++ app/commands/incident.py
@@ -53,6 +53,90 @@ def handle_incident_action_buttons(client, ack, body, logger):
         log_to_sentinel("ignore_incident_button_pressed", body)
 
 
+def incident_modal_view(command, options, locale="en-US"):
+    return {
+        "type": "modal",
+        "callback_id": "incident_view",
+        "title": {"type": "plain_text", "text": "SRE - Start an incident"},
+        "submit": {"type": "plain_text", "text": "Submit"},
+        "blocks": [
+            {
+                "type": "actions",
+                "block_id": "locale",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "language",
+                            "emoji": True,
+                        },
+                        "value": locale,
+                        "action_id": "change_locale",
+                    }
+                ],
+            },
+            {
+                "type": "header",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Congratulations! // Félicitations!",
+                    "emoji": True,
+                },
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "Something has gone wrong. You've got this! // Il y a eu un problème. Vous pouvez y arriver!",
+                },
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "This app is going to help you get set up. It will create the following: \n \n • a channel \n • an incident report \n • a Google Meet\n\n--\n\nCette application vous aidera à vous préparer. Elle créera les choses suivantes: \n \n • un canal \n • un rapport d'incident \n • une rencontre Google Meet\n\n",
+                },
+            },
+            {"type": "divider"},
+            {
+                "type": "section",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Fill out the two fields below and you are good to go // Remplissez les deux champs ici-bas et vous pourrez commencer:",
+                },
+            },
+            {
+                "block_id": "name",
+                "type": "input",
+                "element": {
+                    "type": "plain_text_input",
+                    "action_id": "name",
+                    "initial_value": command["text"],
+                },
+                "label": {
+                    "type": "plain_text",
+                    "text": "Short description (ex: too many 500 errors) | Courte description",
+                },
+            },
+            {
+                "block_id": "product",
+                "type": "input",
+                "element": {
+                    "type": "static_select",
+                    "placeholder": {
+                        "type": "plain_text",
+                        "text": "Select a product | Choisissez un produit",
+                    },
+                    "options": options,
+                    "action_id": "product",
+                },
+                "label": {"type": "plain_text", "text": "Product", "emoji": True},
+            },
+        ],
+    }
+
+
 def open_modal(client, ack, command, body):
     ack()
     folders = google_drive.list_folders()
@@ -63,93 +147,11 @@ def open_modal(client, ack, command, body):
         }
         for i in folders
     ]
-    user_id = body["user"]["id"]
+    print(client)
+    user_id = body["user_id"]
     locale = get_user_locale(user_id, client)
-
-    client.views_open(
-        trigger_id=body["trigger_id"],
-        view={
-            "type": "modal",
-            "callback_id": "incident_view",
-            "title": {"type": "plain_text", "text": "SRE - Start an incident"},
-            "submit": {"type": "plain_text", "text": "Submit"},
-            "blocks": [
-                {
-                    "type": "actions",
-                    "block_id": "locale",
-                    "elements": [
-                        {
-                            "type": "button",
-                            "text": {
-                                "type": "plain_text",
-                                "text": "language",
-                                "emoji": True,
-                            },
-                            "value": locale,
-                            "action_id": "atip_change_locale",
-                        }
-                    ],
-                },
-                {
-                    "type": "header",
-                    "text": {
-                        "type": "plain_text",
-                        "text": "Congratulations! // Félicitations!",
-                        "emoji": True,
-                    },
-                },
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "Something has gone wrong. You've got this! // Il y a eu un problème. Vous pouvez y arriver!",
-                    },
-                },
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "This app is going to help you get set up. It will create the following: \n \n • a channel \n • an incident report \n • a Google Meet\n\n--\n\nCette application vous aidera à vous préparer. Elle créera les choses suivantes: \n \n • un canal \n • un rapport d'incident \n • une rencontre Google Meet\n\n",
-                    },
-                },
-                {"type": "divider"},
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "plain_text",
-                        "text": "Fill out the two fields below and you are good to go // Remplissez les deux champs ici-bas et vous pourrez commencer:",
-                    },
-                },
-                {
-                    "block_id": "name",
-                    "type": "input",
-                    "element": {
-                        "type": "plain_text_input",
-                        "action_id": "name",
-                        "initial_value": command["text"],
-                    },
-                    "label": {
-                        "type": "plain_text",
-                        "text": "Short description (ex: too many 500 errors) | Courte description",
-                    },
-                },
-                {
-                    "block_id": "product",
-                    "type": "input",
-                    "element": {
-                        "type": "static_select",
-                        "placeholder": {
-                            "type": "plain_text",
-                            "text": "Select a product | Choisissez un produit",
-                        },
-                        "options": options,
-                        "action_id": "product",
-                    },
-                    "label": {"type": "plain_text", "text": "Product", "emoji": True},
-                },
-            ],
-        },
-    )
+    view = incident_modal_view(command, options, locale)
+    client.views_open(trigger_id=body["trigger_id"], view=view)
 
 
 def submit(ack, view, say, body, client, logger):
