4fb1fbd0877cf11ea1a826256bb602d49c110f10
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2022-12-13 14:50:52 +0000

    feat: test creator added to incident channel

--- app/tests/commands/test_incident.py
+++ app/tests/commands/test_incident.py
@@ -435,6 +435,33 @@ def test_incident_submit_creates_channel_sets_topic_and_announces_channel(
 @patch("commands.incident.google_drive.create_new_incident")
 @patch("commands.incident.google_drive.list_metadata")
 @patch("commands.incident.log_to_sentinel")
+def test_incident_submit_adds_creator_to_channel(
+    _log_to_sentinel_mock,
+    _mock_list_metadata,
+    _mock_create_new_incident,
+    _mock_merge_data,
+    _mock_update_incident_list,
+):
+    ack = MagicMock()
+    logger = MagicMock()
+    view = helper_generate_view()
+    say = MagicMock()
+    body = {"user": {"id": "user_id"}}
+    client = MagicMock()
+    client.conversations_create.return_value = {
+        "channel": {"id": "channel_id", "name": "channel_name"}
+    }
+    incident.submit(ack, view, say, body, client, logger)
+    client.conversations_invite.assert_called_once_with(
+        channel="channel_id", users="user_id"
+    )
+
+
+@patch("commands.incident.google_drive.update_incident_list")
+@patch("commands.incident.google_drive.merge_data")
+@patch("commands.incident.google_drive.create_new_incident")
+@patch("commands.incident.google_drive.list_metadata")
+@patch("commands.incident.log_to_sentinel")
 def test_incident_submit_truncates_meet_link_if_too_long(
     _log_to_sentinel_mock,
     _mock_list_metadata,
