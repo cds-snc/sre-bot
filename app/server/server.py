d2d2335f1d7f4b1b234eecf04d9d4becc6ad0874
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2023-05-18 16:45:32 +0000

    Making the checking of array more pythonic

--- app/server/server.py
+++ app/server/server.py
@@ -112,7 +112,7 @@ def handle_webhook(id: str, payload: WebhookPayload | str, request: Request):
                 blocks = aws.parse(payload, request.state.bot.client)
                 # if we have an empty message, log that we have an empty
                 # message and return without posting to slack
-                if blocks is None or len(blocks) == 0:
+                if not blocks:
                     logging.info("No blocks to post, returning")
                     return
                 payload = WebhookPayload(blocks=blocks)
