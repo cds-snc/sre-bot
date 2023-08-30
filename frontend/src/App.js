79bd7d966886181db8b08a2a787a196d856b921f
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2023-08-30 14:06:32 -0700

    Initial React frontend implementation (Admin UI) (#244)
    * Adding initial react structure
    
    * Adding initial React frontend
    
    * Removing logo
    
    * Modifying gitignore
    
    * Modifying gitignore
    
    * Deleting unecesary css components

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src = "sre_bot_logo.png" alt="sre_bot"></img>
+        <h1>SRE Bot Admin</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
