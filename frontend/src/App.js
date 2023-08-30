6ca39e224f00e6c8acbb5f24176d7951b62d2f3b
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2023-08-30 00:12:45 +0000

    Adding initial React frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src = "sre_bot_logo.png" alt="sre_bot"></img>
+        <h1>SRE Bot Admin</h1>
       </header>
     </div>
   );
