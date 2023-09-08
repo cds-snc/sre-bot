414da842869491c5fdfdda86e94ee9dd0facfc37
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2023-09-08 22:35:18 +0000

    Removing a comment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src = "sre_bot_logo.png" alt="sre_bot"></img> */}
         <img src = {sre_bot_logo} alt="sre_bot"></img>
         <h1>SRE Bot Admin</h1>
       </header>
