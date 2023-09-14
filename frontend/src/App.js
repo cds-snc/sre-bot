43db599c9d1b31836232a87e2117ca44e3f963ca
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2023-09-14 22:49:29 +0000

    Committing initial dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,34 @@
-import './App.css';
+// import './App.css';
 import sre_bot_logo from './sre_bot_logo.png';
 
-
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import React, { Component } from 'react';
+// import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import LandingPage from './pages/LandingPage.js';
 import HomePage from './pages/HomePage.js';
+import Dashboard from './components/Dashboard';
 
-function App() {
-  return (
-  <div className="hero min-h-screen bg-base-200">
+// function App() {
+//   return (
+//   <div className="hero min-h-screen bg-base-200">
       
-  <div className="hero-content flex-col lg:flex-row">
+//   <div className="hero-content flex-col lg:flex-row">
     
-    <img src = {sre_bot_logo} alt="sre_bot"></img>
-    <div>
-      <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<LandingPage/>} />
-            <Route path="/home" element={<HomePage/>} />
-          </Routes>
-       </BrowserRouter>
-    </div>
-  </div>
-</div>
-  );
-}
+//     <img src = {sre_bot_logo} alt="sre_bot"></img>
+//     <div>
+//       <Dashboard />
+//       {/* <BrowserRouter>
+//           <Routes>
+//             {/* <Route path="/" element={<LandingPage/>} /> */}
+//             {/* <Route path="/home" element={<HomePage/>} /> */}
+//             {/* <Route path="/" element={<Dashboard/>} />
+//           </Routes>
+//        </BrowserRouter> */}
+//     </div>
+//   </div>
+// </div>
+//   );
+// }
+
+const App = () => <Dashboard />;
 
 export default App;
