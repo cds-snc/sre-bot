4ddf5bc10f240117c5c1038101f2d9d672cf9947
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2023-09-15 04:16:16 +0000

    Added dashboard components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,56 @@
-// import './App.css';
-import sre_bot_logo from './sre_bot_logo.png';
+import Topbar from "./scenes/global/Topbar";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
-import React, { Component } from 'react';
-// import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import LandingPage from './pages/LandingPage.js';
-import HomePage from './pages/HomePage.js';
-import Dashboard from './components/Dashboard';
+function App() {
 
-// function App() {
-//   return (
-//   <div className="hero min-h-screen bg-base-200">
-      
-//   <div className="hero-content flex-col lg:flex-row">
-    
-//     <img src = {sre_bot_logo} alt="sre_bot"></img>
-//     <div>
-//       <Dashboard />
-//       {/* <BrowserRouter>
-//           <Routes>
-//             {/* <Route path="/" element={<LandingPage/>} /> */}
-//             {/* <Route path="/home" element={<HomePage/>} /> */}
-//             {/* <Route path="/" element={<Dashboard/>} />
-//           </Routes>
-//        </BrowserRouter> */}
-//     </div>
-//   </div>
-// </div>
-//   );
-// }
-
-const App = () => <Dashboard />;
+  // get the theme and the color mode
+  const [theme, colorMode] = useMode();
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className = "app">
+          <main className="content">
+            <Topbar />
+          </main>
+        </div>
+        {/* <Dashboard /> */}
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  ) 
+}
 
 export default App;
+// import sre_bot_logo from './sre_bot_logo.png';
+
+// import React, { Component } from 'react';
+// import LandingPage from './pages/LandingPage.js';
+// import HomePage from './pages/HomePage.js';
+// import Dashboard from './components/Dashboard';
+
+// // function App() {
+// //   return (
+// //   <div className="hero min-h-screen bg-base-200">
+      
+// //   <div className="hero-content flex-col lg:flex-row">
+    
+// //     <img src = {sre_bot_logo} alt="sre_bot"></img>
+// //     <div>
+// //       <Dashboard />
+// //       {/* <BrowserRouter>
+// //           <Routes>
+// //             {/* <Route path="/" element={<LandingPage/>} /> */}
+// //             {/* <Route path="/home" element={<HomePage/>} /> */}
+// //             {/* <Route path="/" element={<Dashboard/>} />
+// //           </Routes>
+// //        </BrowserRouter> */}
+// //     </div>
+// //   </div>
+// // </div>
+// //   );
+// // }
+
+// const App = () => <Dashboard />;
+
+// export default App;
