f875a9bcee46256c3ecc2f211b3ae27e964205f1
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2024-02-01 19:45:00 +0000

    feat: use new drive module functions

--- app/commands/google_service.py
+++ app/commands/google_service.py
@@ -1,7 +1,8 @@
 """Testing new google service (will be removed)"""
 import os
 
-from integrations.google_workspace.google_service import get_google_service
+# from integrations.google_workspace.google_service import get_google_service
+from integrations.google_workspace import google_drive
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -30,20 +31,4 @@ def google_service_command(client, body):
 
 
 def list_folders():
-    service = get_google_service("drive", "v3")
-    results = (
-        service.files()
-        .list(
-            pageSize=25,
-            supportsAllDrives=True,
-            includeItemsFromAllDrives=True,
-            corpora="drive",
-            q="parents in '{}' and mimeType = 'application/vnd.google-apps.folder' and trashed=false and not name contains '{}'".format(
-                SRE_INCIDENT_FOLDER, "Templates"
-            ),
-            driveId=SRE_DRIVE_ID,
-            fields="nextPageToken, files(id, name)",
-        )
-        .execute()
-    )
-    return results.get("files", [])
+    return google_drive.list_folders_in_folder(SRE_INCIDENT_FOLDER)
