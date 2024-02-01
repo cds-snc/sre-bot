4355ba863a552ceeb06fd34308d39101eede2864
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2024-02-01 15:02:42 +0000

    feat: make test command available in dev only

--- app/commands/sre.py
+++ app/commands/sre.py
@@ -21,6 +21,8 @@ help_text = """
 \n      - show the version of the SRE Bot
 \n      - montre la version du bot SRE"""
 
+PREFIX = os.environ.get("PREFIX", "")
+
 
 def sre_command(ack, command, logger, respond, client, body):
     ack()
@@ -48,7 +50,11 @@ def sre_command(ack, command, logger, respond, client, body):
         case "version":
             respond(f"SRE Bot version: {os.environ.get('GIT_SHA', 'unknown')}")
         case "google-service":
-            google_service.google_service_command(client, body)
+            if PREFIX == "dev-":
+                google_service.google_service_command(client, body)
+            else:
+                respond("This command is only available in the dev environment.")
+            return
         case _:
             respond(
                 f"Unknown command: `{action}`. Type `/sre help` to see a list of commands. \nCommande inconnue: `{action}`. Entrez `/sre help` pour une liste des commandes valides"
