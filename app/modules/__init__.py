8b9785b58fca9fd7dcdc1bb7db462a4c4cc47533
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2024-02-12 19:54:52 +0000

    fix: geolocate helper import

--- app/modules/__init__.py
+++ app/modules/__init__.py
@@ -2,4 +2,3 @@ from .atip import atip  # noqa: F401
 from .aws import aws  # noqa: F401
 from .secret import secret  # noqa: F401
 from .sre import sre  # noqa: F401
-from .geolocate_helper import geolocate_helper  # noqa: F401
