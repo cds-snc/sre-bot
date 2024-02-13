59ae3bbd463c54af3d6a96c433c5fbed2f3857c3
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2024-02-13 18:33:23 +0000

    feat: use slack integrations to get stale channels

--- app/jobs/notify_stale_incident_channels.py
+++ app/jobs/notify_stale_incident_channels.py
@@ -1,13 +1,15 @@
 from commands.utils import log_to_sentinel
-from commands import utils
+from integrations.slack import channels as slack_channels
 import logging
 
 logging.basicConfig(level=logging.INFO)
 
+INCIDENT_CHANNELS_PATTERN = r"^incident-\d{4}-"
+
 
 def notify_stale_incident_channels(client):
     logging.info("Checking for stale incident channels")
-    channels = utils.get_stale_channels(client)
+    channels = slack_channels.get_stale_channels(client, pattern=INCIDENT_CHANNELS_PATTERN)
     text = """👋  Hi! There have been no updates in this incident channel for 14 days! Consider archiving it.\n
         Bonjour! Il n'y a pas eu de mise à jour dans ce canal d'incident depuis 14 jours. Vous pouvez considérer l'archiver."""
     attachments = [
