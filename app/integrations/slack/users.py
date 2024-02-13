de11e37acd27939a28951bd0c1be7ebaddf89fbc
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2024-02-13 17:12:16 +0000

    feat: add get user id from slack request

--- app/integrations/slack/users.py
+++ app/integrations/slack/users.py
@@ -2,14 +2,45 @@
 
 This module contains the user related functionality for the Slack integration.
 """
+import re
+
+SLACK_USER_ID_REGEX = r"^[A-Z0-9]+$"
 
 
-def get_user_locale(user_id, client):
+def get_user_id_from_request(body):
+    """
+    Returns the user_id from the body of a request, otherwise None.
+      Supports the following formats:
+    - user_id (Slack slash command)
+    - user (Slack Message, Modal, Block)
+    - event.user (Slack Event)
+    """
+    user_id = None
+    if "user_id" in body:
+        user_id = body["user_id"]
+    elif "user" in body:
+        if isinstance(body["user"], dict) and "id" in body["user"]:
+            user_id = body["user"]["id"]
+        else:
+            user_id = body["user"]
+    elif (
+        "event" in body and isinstance(body["event"], dict) and "user" in body["event"]
+    ):
+        user_id = body["event"]["user"]
+    if user_id:
+        match = re.match(SLACK_USER_ID_REGEX, user_id)
+        if match:
+            return match.group(0)
+
+
+def get_user_locale(client, user_id=None):
     """
     Returns the user locale from a command's user_id if valid, "en-US" as default otherwise
     """
     default_locale = "en-US"
     supported_locales = ["en-US", "fr-FR"]
+    if user_id is None:
+        return default_locale
     user_locale = client.users_info(user=user_id, include_locale=True)
     if user_locale["ok"] and (user_locale["user"]["locale"] in supported_locales):
         return user_locale["user"]["locale"]
--- app/tests/integrations/slack/test_users.py
+++ app/tests/integrations/slack/test_users.py
@@ -3,6 +3,31 @@ from unittest.mock import MagicMock
 from integrations.slack import users
 
 
+def test_get_user_id_from_request_with_user_id():
+    body = {"user_id": "U00AAAAAAA0"}
+    assert users.get_user_id_from_request(body) == "U00AAAAAAA0"
+
+
+def test_get_user_id_from_request_with_id():
+    body = {"user": {"id": "U00AAAAAAA0"}}
+    assert users.get_user_id_from_request(body) == "U00AAAAAAA0"
+
+
+def test_get_user_id_from_request_without_id():
+    body = {"user": "U00AAAAAAA0"}
+    assert users.get_user_id_from_request(body) == "U00AAAAAAA0"
+
+
+def test_get_user_id_from_event_request():
+    body = {"event": {"user": "U00AAAAAAA0"}}
+    assert users.get_user_id_from_request(body) == "U00AAAAAAA0"
+
+
+def test_get_user_id_from_invalid_request():
+    body = {"invalid": "invalid"}
+    assert users.get_user_id_from_request(body) is None
+
+
 def test_get_user_locale_supported_locale():
     client = MagicMock()
     user_id = MagicMock()
@@ -10,7 +35,7 @@ def test_get_user_locale_supported_locale():
         "ok": True,
         "user": {"id": "U00AAAAAAA0", "locale": "fr-FR"},
     }
-    assert users.get_user_locale(user_id, client) == "fr-FR"
+    assert users.get_user_locale(client, user_id) == "fr-FR"
 
 
 def test_get_user_locale_unsupported_locale():
@@ -20,11 +45,17 @@ def test_get_user_locale_unsupported_locale():
         "ok": True,
         "user": {"id": "U00AAAAAAA0", "locale": "es-ES"},
     }
-    assert users.get_user_locale(user_id, client) == "en-US"
+    assert users.get_user_locale(client, user_id) == "en-US"
 
 
 def test_get_user_locale_without_locale():
     client = MagicMock()
     user_id = MagicMock()
     client.users_info.return_value = {"ok": False}
-    assert users.get_user_locale(user_id, client) == "en-US"
+    assert users.get_user_locale(client, user_id) == "en-US"
+
+
+def test_get_user_locale_without_user_id():
+    client = MagicMock()
+    user_id = None
+    assert users.get_user_locale(client, user_id) == "en-US"
