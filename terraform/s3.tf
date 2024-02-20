49a0ff788570f31ea16b507052279f13344f60c1
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2024-02-20 12:19:12 -0500

    Feat/aws integration (infra) (#415)
    * feat: add s3 bucket dedicated to SRE bot
    
    * feat: give S3 bucket read/write permission to SRE bot
    
    * fix: fmt
    
    * fix: capital S

---
+++ terraform/s3.tf
@@ -0,0 +1,10 @@
+module "sre_bot_bucket" {
+  source      = "github.com/cds-snc/terraform-modules//S3?ref=v9.2.2"
+  bucket_name = "sre-bot-bucket"
+  versioning = {
+    enabled = true
+  }
+
+  billing_tag_key   = "CostCentre"
+  billing_tag_value = var.billing_code
+}
