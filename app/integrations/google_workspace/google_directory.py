64eed1e374063faa2d0f3a02c35cb241c9140d93
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2024-02-20 14:02:52 +0000

    feat: add groups functions

--- app/integrations/google_workspace/google_directory.py
+++ app/integrations/google_workspace/google_directory.py
@@ -7,6 +7,32 @@ from integrations.google_workspace.google_service import (
 )
 
 GOOGLE_DELEGATED_ADMIN_EMAIL = os.environ.get("GOOGLE_DELEGATED_ADMIN_EMAIL")
+GOOGLE_WORKSPACE_CUSTOMER_ID = os.environ.get("GOOGLE_WORKSPACE_CUSTOMER_ID")
+
+
+@handle_google_api_errors
+def get_user(user_key):
+    """Get a user by user key in the Google Workspace domain.
+
+    reference: https://developers.google.com/admin-sdk/directory/reference/rest/v1/users/get
+
+    Args:
+        user_key (str): The user's primary email address, alias email address, or unique user ID.
+
+    Returns:
+        dict: A user object.
+    """
+
+    scopes = ["https://www.googleapis.com/auth/admin.directory.user.readonly"]
+
+    service = get_google_service(
+        "admin",
+        "directory_v1",
+        delegated_user_email=GOOGLE_DELEGATED_ADMIN_EMAIL,
+        scopes=scopes,
+    )
+    user = service.users().get(userKey=user_key).execute()
+    return user
 
 
 @handle_google_api_errors
@@ -19,12 +45,7 @@ def list_users():
         list: A list of user objects.
     """
 
-    scopes = [
-        "https://www.googleapis.com/auth/admin.directory.user.readonly",
-        "https://www.googleapis.com/auth/admin.directory.group.readonly",
-        "https://www.googleapis.com/auth/admin.directory.group.member.readonly",
-        "https://www.googleapis.com/auth/admin.reports.audit.readonly",
-    ]
+    scopes = ["https://www.googleapis.com/auth/admin.directory.user.readonly"]
 
     service = get_google_service(
         "admin",
@@ -34,7 +55,55 @@ def list_users():
     )
     users = (
         service.users()
-        .list(customer="C03nmsv71", maxResults=500, orderBy="email")
+        .list(customer=GOOGLE_WORKSPACE_CUSTOMER_ID, maxResults=500, orderBy="email")
         .execute()
     )
     return users.get("users", [])
+
+
+@handle_google_api_errors
+def list_groups():
+    """List all groups in the Google Workspace domain.
+
+    reference: https://admin.googleapis.com/admin/directory/v1/groups
+
+    Returns:
+        list: A list of group objects.
+    """
+
+    scopes = ["https://www.googleapis.com/auth/admin.directory.group.readonly"]
+
+    service = get_google_service(
+        "admin",
+        "directory_v1",
+        delegated_user_email=GOOGLE_DELEGATED_ADMIN_EMAIL,
+        scopes=scopes,
+    )
+    groups = (
+        service.groups()
+        .list(customer=GOOGLE_WORKSPACE_CUSTOMER_ID, maxResults=500, orderBy="email")
+        .execute()
+    )
+    return groups.get("groups", [])
+
+
+@handle_google_api_errors
+def list_group_members(group_key):
+    """List all group members in the Google Workspace domain.
+
+    reference: https://admin.googleapis.com/admin/directory/v1/members
+
+    Returns:
+        list: A list of group member objects.
+    """
+
+    scopes = ["https://www.googleapis.com/auth/admin.directory.group.member.readonly"]
+
+    service = get_google_service(
+        "admin",
+        "directory_v1",
+        delegated_user_email=GOOGLE_DELEGATED_ADMIN_EMAIL,
+        scopes=scopes,
+    )
+    group_members = service.members().list(groupKey=group_key, maxResults=500).execute()
+    return group_members.get("members", [])
