32db1aaa122cf5f32783bdb827c49aa6ac2cdeed
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2024-04-25 13:58:47 +0000

    feat: add aws fixtures for consistency

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -79,3 +79,94 @@ def google_groups_w_users(google_groups, google_users):
         return groups
 
     return _google_groups_w_users
+
+
+# AWS API fixtures
+
+
+@pytest.fixture
+def aws_users():
+    def _aws_users(n=3, prefix="", domain="test.com", store_id="d-123412341234"):
+        users = []
+        for i in range(n):
+            user = {
+                "UserName": f"{prefix}_email_{i}@{domain}",
+                "UserId": f"{prefix}_id_{i}",
+                "Name": {
+                    "FamilyName": f"Family_name_{i}",
+                    "GivenName": f"Given_name_{i}",
+                },
+                "DisplayName": f"Given_name_{i} Family_name_{i}",
+                "Emails": [
+                    {
+                        "Value": f"{prefix}_email_{i}@{domain}",
+                        "Type": "work",
+                        "Primary": True,
+                    }
+                ],
+                "IdentityStoreId": f"{store_id}",
+            }
+            users.append(user)
+        return users
+
+    return _aws_users
+
+
+@pytest.fixture
+def aws_groups():
+    def _aws_groups(n=3, prefix="", domain="test.com", store_id="d-123412341234"):
+        return {
+            "Groups": [
+                {
+                    "GroupId": f"{prefix}_aws-group_id{i+1}",
+                    "DisplayName": f"AWS-group{i+1}",
+                    "Description": f"A group to test resolving AWS-group{i+1} memberships",
+                    "IdentityStoreId": f"{store_id}",
+                }
+                for i in range(n)
+            ]
+        }
+
+    return _aws_groups
+
+
+@pytest.fixture
+def aws_groups_memberships():
+    def _aws_groups_memberships(
+        n=3, prefix="", domain="test.com", store_id="d-123412341234"
+    ):
+        return {
+            "GroupMemberships": [
+                {
+                    "IdentityStoreId": f"{store_id}",
+                    "MembershipId": f"{prefix}_membership_id_{i+1}",
+                    "GroupId": f"{prefix}_aws-group_id{i+1}",
+                    "MemberId": {
+                        "UserId": f"{prefix}_id_{i}",
+                    },
+                }
+                for i in range(n)
+            ]
+        }
+
+    return _aws_groups_memberships
+
+
+@pytest.fixture
+def aws_groups_w_users(aws_groups, aws_users, aws_groups_memberships):
+    def _aws_groups_w_users(
+        n_groups=1, n_users=3, prefix="", domain="test.com", store_id="d-123412341234"
+    ):
+        groups = aws_groups(n_groups, prefix, domain, store_id)["Groups"]
+        users = aws_users(n_users, prefix, domain, store_id)
+        memberships = aws_groups_memberships(n_groups, prefix, domain, store_id)[
+            "GroupMemberships"
+        ]
+        for group, membership in zip(groups, memberships):
+            group.update(membership)
+            group["GroupMemberships"] = [
+                {**membership, "MemberId": user} for user in users
+            ]
+        return groups
+
+    return _aws_groups_w_users
