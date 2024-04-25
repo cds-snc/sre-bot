b8edfc141f7247f300123d5a171c200c94783948
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2024-04-25 20:24:14 +0000

    feat: Refactor provisioning/groups.py to support integration from ext sources

---
+++ app/modules/provisioning/groups.py
@@ -0,0 +1,34 @@
+from integrations.google_workspace import google_directory
+from integrations.aws import identity_store
+from modules.utils import filters as filter_tools
+
+
+def get_groups_with_members_from_integration(integration_source, **kwargs):
+    """Retrieve the users from an integration group source.
+    Supported sources are:
+    - Google Groups
+    - AWS Identity Center (Identity Store)
+
+    Args:
+        integration_source (str): The source of the groups.
+    """
+    filters = kwargs.get("filters", [])
+    query = kwargs.get("query", None)
+    members_details = kwargs.get("members_details", True)
+
+    groups = []
+    match integration_source:
+        case "google_groups":
+            groups = google_directory.list_groups_with_members(
+                query=query, members_details=members_details
+            )
+        case "aws_identity_center":
+            groups = identity_store.list_groups_with_memberships(
+                members_details=members_details
+            )
+        case _:
+            return groups
+
+    for filter in filters:
+        groups = filter_tools.filter_by_condition(groups, filter)
+    return groups
