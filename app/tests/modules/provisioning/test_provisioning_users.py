9225fee20e5a83e5f0b319f60086c5cc7a585f49
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2024-04-25 23:08:22 +0000

    fix: switch to conftest fixtures for mocked data

--- app/tests/modules/provisioning/test_provisioning_users.py
+++ app/tests/modules/provisioning/test_provisioning_users.py
@@ -1,121 +1,5 @@
 # from unittest.mock import patch
-from pytest import fixture
-
 from modules.provisioning import users
-
-
-@fixture
-def source_groups():
-    return [
-        {
-            "id": "source_group_id1",
-            "name": "SOURCE-group1",
-            "email": "SOURCE-group1@test.com",
-            "members": [
-                {"email": "user1.test@test.com", "id": "user1_id"},
-                {"email": "user2.test@test.com", "id": "user2_id"},
-                {"email": "user3.test@test.com", "id": "user3_id"},
-            ],
-        },
-        {
-            "id": "source_group_id2",
-            "name": "SOURCE-group2",
-            "email": "SOURCE-group2@test.com",
-            "members": [
-                {"email": "user1.test@test.com", "id": "user1_id"},
-                {"email": "user2.test@test.com", "id": "user2_id"},
-                {"email": "user9.test@external.com", "id": "user9_external_id"},
-            ],
-        },
-        {
-            "id": "source_group_id3",
-            "name": "SOURCEgroup3",
-            "email": "sourcegroup3@test.com",
-        },
-    ]
-
-
-def target_groups():
-    return [
-        {
-            "GroupId": "target_group_id1",
-            "DisplayName": "group1",
-            "Memberships": [
-                {
-                    "Email": [{"user1.test@test.com"}],
-                    "id": "user1_id",
-                    "Name": {"GivenName": "User1", "FamilyName": "Test"},
-                },
-            ],
-        },
-        {
-            "GroupId": "target_group_id2",
-            "DisplayName": "group2",
-        },
-    ]
-
-
-@fixture
-def source_users():
-    return {
-        "users": [
-            {
-                "email": "user1.test@test.com",
-                "id": "user1_id",
-                "name": {
-                    "givenName": "User1",
-                    "familyName": "Test",
-                    "fullName": "User1 Test",
-                },
-            },
-            {
-                "email": "user2.test@test.com",
-                "id": "user2_id",
-                "name": {
-                    "givenName": "User2",
-                    "familyName": "Test",
-                    "fullName": "User2 Test",
-                },
-            },
-            {
-                "email": "user3.test@test.com",
-                "id": "user3_id",
-                "name": {
-                    "givenName": "User3",
-                    "familyName": "Test",
-                    "fullName": "User3 Test",
-                },
-            },
-            {
-                "email": "user9.test@external.com",
-                "id": "user9_external_id",
-                "name": {
-                    "givenName": "User9",
-                    "familyName": "Test",
-                    "fullName": "User9 Test",
-                },
-            },
-        ],
-        "key": "email",
-    }
-
-
-@fixture
-def target_users():
-    return {
-        "users": [
-            {
-                "UserName": "user1.test@test.com",
-                "UserId": "user1_id",
-                "Name": {"GivenName": "User1", "FamilyName": "Test"},
-                "DisplayName": "User1 Test",
-                "Emails": [
-                    {"Value": "user1.test@test.com", "Type": "work", "Primary": True}
-                ],
-            },
-        ],
-        "key": "UserName",
-    }
 
 
 def test_get_unique_users_from_groups(google_groups_w_users):
@@ -169,38 +53,13 @@ def test_get_unique_users_from_dict_group_with_duplicate_key():
     )
 
 
-def test_users_without_filters(source_users, target_users):
+def test_users_without_filters(google_users, aws_users):
+    source_users = {"users": google_users(4, "user", "test.com"), "key": "primaryEmail"}
+    target_users = {"users": aws_users(3, "user", "test.com"), "key": "UserName"}
+
     users_to_create, users_to_delete = users.sync(source_users, target_users)
 
-    assert users_to_create == [
-        {
-            "email": "user2.test@test.com",
-            "id": "user2_id",
-            "name": {
-                "givenName": "User2",
-                "familyName": "Test",
-                "fullName": "User2 Test",
-            },
-        },
-        {
-            "email": "user3.test@test.com",
-            "id": "user3_id",
-            "name": {
-                "givenName": "User3",
-                "familyName": "Test",
-                "fullName": "User3 Test",
-            },
-        },
-        {
-            "email": "user9.test@external.com",
-            "id": "user9_external_id",
-            "name": {
-                "givenName": "User9",
-                "familyName": "Test",
-                "fullName": "User9 Test",
-            },
-        },
-    ]
+    assert users_to_create == [source_users["users"][3]]
     assert users_to_delete == []
 
 
@@ -225,21 +84,25 @@ def test_users_with_filters(google_users, aws_users):
     assert users_to_delete == []
 
 
-def test_users_with_empty_source(source_users, target_users):
-    source_users["users"] = []
+def test_users_with_empty_source(aws_users):
+    source_users = {"users": [], "key": "primaryEmail"}
+    target_users = {"users": aws_users(3, "user", "test.com"), "key": "UserName"}
     users_to_create, users_to_delete = users.sync(source_users, target_users)
     assert users_to_create == []
     assert users_to_delete == []
 
 
-def test_users_with_empty_target(source_users, target_users):
-    target_users["users"] = []
+def test_users_with_empty_target(google_users):
+    source_users = {"users": google_users(4, "user", "test.com"), "key": "primaryEmail"}
+    target_users = {"users": [], "key": "UserName"}
     users_to_create, users_to_delete = users.sync(source_users, target_users)
     assert users_to_create == source_users["users"]
     assert users_to_delete == []
 
 
-def test_users_with_delete_target_all(source_users, target_users):
+def test_users_with_delete_target_all(aws_groups):
+    source_users = {"users": [], "key": "primaryEmail"}
+    target_users = {"users": aws_groups(3, "user", "test.com"), "key": "UserName"}
     users_to_create, users_to_delete = users.sync(
         source_users, target_users, delete_target_all=True
     )
