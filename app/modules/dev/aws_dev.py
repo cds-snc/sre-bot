8c68f8c1b9c056723e194b688e2d1c0ddf2b7b7c
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2024-05-07 19:58:59 +0000

    feat: make the output of the sync command formatted for the slack bot

--- app/modules/dev/aws_dev.py
+++ app/modules/dev/aws_dev.py
@@ -1,5 +1,4 @@
 """Testing AWS service (will be removed)"""
-import json
 import logging
 from modules.aws import identity_center
 
@@ -12,9 +11,27 @@ logger = logging.getLogger(__name__)
 
 def aws_dev_command(ack, client, body, respond):
     ack()
-    response = identity_center.synchronize(enable_groups_sync=True)
+    response = identity_center.synchronize(enable_groups_sync=False)
     if not response:
         respond("No groups found.")
     else:
-        logger.info(json.dumps(response, indent=2))
-        # respond(json.dumps(response[0], indent=2))
+        message = ""
+        if identity_center.DRY_RUN:
+            message += "Dry run mode enabled.\n"
+        if response["users"]:
+            users_created, users_deleted = response["users"]
+            message += "Users created:\n- " + "\n- ".join(users_created) + "\n"
+            message += "Users deleted:\n- " + "\n- ".join(users_deleted) + "\n"
+        else:
+            message += "Users Sync Disabled.\n"
+        if response["groups"]:
+            groups_created, groups_deleted = response["groups"]
+            message += (
+                "Groups memberships created:\n- " + "\n- ".join(groups_created) + "\n"
+            )
+            message += (
+                "Groups memberships deleted:\n- " + "\n- ".join(groups_deleted) + "\n"
+            )
+        else:
+            message += "Groups Sync Disabled.\n"
+        respond(message)
