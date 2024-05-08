d3408e4c24ca23bcfa312aa51273f3181b659824
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2024-05-08 21:02:16 +0000

    feat: setup generic provisioning for users

--- app/modules/provisioning/users.py
+++ app/modules/provisioning/users.py
@@ -1,9 +1,12 @@
-from logging import getLogger
+# from logging import getLogger
+import logging
 
 from utils import filters as filter_tools
 
 
-logger = getLogger(__name__)
+logger = logging.getLogger(__name__)
+
+DISPLAY_KEYS = {"aws": "UserName", "google": "primaryEmail"}
 
 
 def get_unique_users_from_groups(groups, key):
@@ -31,3 +34,35 @@ def get_unique_users_from_groups(groups, key):
                 users_dict[str(user)] = user
     logger.info(f"Found {len(users_dict)} unique users.")
     return list(users_dict.values())
+
+
+def provision_users(
+    integration, operation, function, users, display_key=None, **kwargs
+):
+    """Provision users in the specified integration's operation.
+
+    Args:
+        integration (str): The target integration intended for the users.
+        operation (str): The operation to perform on the users.
+        function (function): The function to perform on the users.
+        users (list): A list of user objects to create.
+        display_key (str): The key to display the user name.
+
+    Returns:
+        list: A list of created users objects.
+    """
+    provisioned_users = []
+    logger.info(f"{integration}:Starting {operation} of {len(users)} user(s)")
+    for user in users:
+        logger.info(f"user's data:\n{user}")
+        response = function(**user, **kwargs)
+        if response:
+            logger.info(
+                f"{integration}:Successful {operation} of user {user[display_key] if display_key else user}"
+            )
+            provisioned_users.append({"user": user, "response": response})
+        else:
+            logger.error(
+                f"{integration}:Failed {operation} user {user[display_key] if display_key else user}"
+            )
+    return provisioned_users
--- app/tests/modules/provisioning/test_provisioning_users.py
+++ app/tests/modules/provisioning/test_provisioning_users.py
@@ -1,4 +1,4 @@
-# from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 from modules.provisioning import users
 
 
@@ -51,3 +51,57 @@ def test_get_unique_users_from_dict_group_with_duplicate_key():
     assert sorted(users_from_groups, key=lambda user: user["id"]) == sorted(
         expected_users, key=lambda user: user["id"]
     )
+
+
+@patch("modules.provisioning.users.logger")
+def test_provision_users_success(mock_logger):
+    users_list = [{"name": "user1"}, {"name": "user2"}, {"name": "user3"}]
+    mock_function = MagicMock()
+    mock_function.return_value = True
+
+    result = users.provision_users("aws", "creation", mock_function, users_list, "name")
+
+    assert len(result) == len(users_list)
+    assert mock_function.call_count == len(users_list)
+    mock_logger.info.assert_any_call("aws:Starting creation of 3 user(s)")
+    for user in users_list:
+        mock_logger.info.assert_any_call(f"user's data:\n{user}")
+        mock_logger.info.assert_any_call(
+            f"aws:Successful creation of user {user['name']}"
+        )
+    mock_logger.error.assert_not_called()
+
+
+@patch("modules.provisioning.users.logger")
+def test_provision_users_failure(mock_logger):
+    mock_function = MagicMock()
+    users_list = [{"name": "user1"}, {"name": "user2"}, {"name": "user3"}]
+    mock_function.side_effect = [True, False, True]
+
+    result = users.provision_users("aws", "creation", mock_function, users_list, "name")
+
+    assert len(result) == 2
+    assert mock_function.call_count == len(users_list)
+    mock_logger.info.assert_any_call("aws:Starting creation of 3 user(s)")
+    for i in range(len(users_list)):
+        mock_logger.info.assert_any_call(f"user's data:\n{users_list[i]}")
+        if i == 1:
+            mock_logger.error.assert_any_call(
+                f"aws:Failed creation user {users_list[i]['name']}"
+            )
+        else:
+            mock_logger.info.assert_any_call(
+                f"aws:Successful creation of user {users_list[i]['name']}"
+            )
+
+
+@patch("modules.provisioning.users.logger")
+def test_provision_users_empty_list(mock_logger):
+    users_list = []
+    mock_function = MagicMock()
+    result = users.provision_users("aws", "creation", mock_function, users_list, "name")
+
+    assert len(result) == 0
+    assert mock_function.call_count == 0
+    mock_logger.info.assert_any_call("aws:Starting creation of 0 user(s)")
+    mock_logger.error.assert_not_called()
