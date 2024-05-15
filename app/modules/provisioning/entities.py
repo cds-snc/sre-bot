a0423d301b5a7d207fe79df34ae31fa8c77ce9a1
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2024-05-15 20:26:08 +0000

    feat: create generic entity provisioning function

---
+++ app/modules/provisioning/entities.py
@@ -0,0 +1,65 @@
+import logging
+from utils import filters
+
+
+logger = logging.getLogger(__name__)
+
+
+def provision_entities(
+    function,
+    entities,
+    execute=True,
+    integration_name="Unspecified",
+    operation_name="Processing",
+    entity_name="Entity",
+    display_key=None,
+    **kwargs,
+):
+    """Provision entities in the specified integration's operation.
+
+    Args:
+        function (function): The function to execute for each entity.
+        entities (list): The list of entities to provision.
+        execute (bool, optional): Whether to execute the operation. Defaults to True.
+        integration_name (str, optional): The name of the integration. Defaults to "unspecified".
+        operation_name (str, optional): The name of the operation. Defaults to "processing".
+        entity_name (str, optional): The name of the entity. Defaults to "entity(ies)".
+        display_key (str, optional): The key to display in the logs. Defaults to None.
+        **kwargs: Additional keyword arguments to pass to the function.
+
+    Returns:
+        list: A list of created entities objects.
+    """
+    provisioned_entities = []
+    if not entities:
+        logger.info(
+            f"{integration_name}:{entity_name}:{operation_name}: No entities to process"
+        )
+        return provisioned_entities
+    logger.info(
+        f"{integration_name}:{entity_name}:{operation_name}: Started processing {len(entities)} entities"
+    )
+    for entity in entities:
+        entity_string = (
+            filters.get_nested_value(entity, display_key) if display_key else entity
+        )
+        if execute:
+            response = function(**entity, **kwargs)
+            if response:
+                logger.info(
+                    f"{integration_name}:{entity_name}:{operation_name}:Successful: {entity_string}"
+                )
+                provisioned_entities.append({"entity": entity, "response": response})
+            else:
+                logger.error(
+                    f"{integration_name}:{entity_name}:{operation_name}:Failed: {entity_string}"
+                )
+        else:
+            logger.info(
+                f"{integration_name}:{entity_name}:{operation_name}:Successful:DRY_RUN: {entity_string}"
+            )
+            provisioned_entities.append({"entity": entity, "response": None})
+    logger.info(
+        f"{integration_name}:{entity_name}:{operation_name}: Completed processing {len(provisioned_entities)} entities"
+    )
+    return provisioned_entities
---
+++ app/tests/modules/provisioning/test_provisioning_entities.py
@@ -0,0 +1,81 @@
+from unittest.mock import MagicMock, call, patch
+from modules.provisioning import entities
+
+
+@patch("modules.provisioning.entities.logger")
+def test_provision_entities_success(mock_logger):
+    users_list = [{"name": "user1"}, {"name": "user2"}, {"name": "user3"}]
+    mock_function = MagicMock()
+    mock_function.return_value = True
+
+    result = entities.provision_entities(
+        mock_function,
+        users_list,
+        integration_name="aws",
+        operation_name="creation",
+        display_key="name",
+    )
+
+    assert len(result) == len(users_list)
+    assert mock_function.call_count == len(users_list)
+    info_calls = [
+        call("aws:Entity:creation: Started processing 3 entities"),
+        call("aws:Entity:creation: Completed processing 3 entities"),
+    ]
+    for i, user in enumerate(users_list):
+        pos = i + 1
+        info_calls.insert(pos, call(f"aws:Entity:creation:Successful: {user['name']}"))
+
+    assert mock_logger.info.call_args_list == info_calls
+    mock_logger.error.assert_not_called()
+
+
+@patch("modules.provisioning.entities.logger")
+def test_provision_entities_failure(mock_logger):
+    mock_function = MagicMock()
+    users_list = [{"name": "user1"}, {"name": "user2"}, {"name": "user3"}]
+    mock_function.side_effect = [True, False, True]
+
+    result = entities.provision_entities(
+        mock_function,
+        users_list,
+        integration_name="aws",
+        operation_name="creation",
+        display_key="name",
+    )
+
+    assert len(result) == 2
+    assert mock_function.call_count == len(users_list)
+
+    info_calls = [
+        call("aws:Entity:creation: Started processing 3 entities"),
+        call("aws:Entity:creation:Successful: user1"),
+        call("aws:Entity:creation:Successful: user3"),
+        call("aws:Entity:creation: Completed processing 2 entities"),
+    ]
+
+    assert mock_logger.info.call_args_list == info_calls
+    assert mock_logger.error.call_args_list == [
+        call("aws:Entity:creation:Failed: user2"),
+    ]
+
+
+@patch("modules.provisioning.entities.logger")
+def test_provision_entities_empty_list(mock_logger):
+    users_list = []
+    mock_function = MagicMock()
+    result = entities.provision_entities(
+        mock_function,
+        users_list,
+        integration_name="aws",
+        operation_name="creation",
+        display_key="name",
+    )
+
+    assert len(result) == 0
+    assert mock_function.call_count == 0
+    assert (
+        call("aws:Entity:creation: No entities to process")
+        in mock_logger.info.call_args_list
+    )
+    mock_logger.error.assert_not_called()
