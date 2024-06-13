5e118a2e70450758bedfb685f8137f771d03615b
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2024-06-13 16:08:09 +0000

    feat: handle kwargs not supported by the method

--- app/integrations/google_workspace/google_service.py
+++ app/integrations/google_workspace/google_service.py
@@ -23,6 +23,7 @@ from google.oauth2 import service_account  # type: ignore
 from googleapiclient.discovery import build  # type: ignore
 from googleapiclient.errors import HttpError, Error  # type: ignore
 from google.auth.exceptions import RefreshError  # type: ignore
+from integrations.utils.api import convert_kwargs_to_camel_case
 
 # Define the default arguments
 DEFAULT_DELEGATED_ADMIN_EMAIL = os.environ.get("GOOGLE_DELEGATED_ADMIN_EMAIL")
@@ -78,7 +79,12 @@ def handle_google_api_errors(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         try:
-            return func(*args, **kwargs)
+            result, unsupported_params = func(*args, **kwargs)
+            if unsupported_params:
+                logging.warning(
+                    f"Unsupported parameters in '{func.__name__}' were filtered out: {', '.join(unsupported_params)}"
+                )
+            return result
         except HttpError as e:
             logging.error(f"An HTTP error occurred in function '{func.__name__}': {e}")
         except ValueError as e:
@@ -124,16 +130,53 @@ def execute_google_api_call(
     service = get_google_service(service_name, version, delegated_user_email, scopes)
     resource_obj = getattr(service, resource)()
     api_method = getattr(resource_obj, method)
+    supported_params = get_google_api_command_parameters(service, resource, method)
+    if kwargs:
+        formatted_kwargs = convert_kwargs_to_camel_case(kwargs)
+    filtered_params = {
+        k: v for k, v in formatted_kwargs.items() if k in supported_params
+    }
+    unsupported_params = set(formatted_kwargs.keys()) - set(filtered_params.keys())
+
     if paginate:
         all_results = []
-        request = api_method(**kwargs)
+        request = api_method(**filtered_params)
         while request is not None:
             results = request.execute()
             if results is not None:
-                all_results.extend(
-                    results.get(resource, [])
-                )  # Use the resource name instead of "users"
+                all_results.extend(results.get(resource, []))
             request = getattr(resource_obj, method + "_next")(request, results)
-        return all_results
+        return all_results, unsupported_params
     else:
-        return api_method(**kwargs).execute()
+        return api_method(**filtered_params).execute(), unsupported_params
+
+
+def get_google_api_command_parameters(service, resource, method):
+    """
+    Get the parameter names for a Google API command, excluding non-parameter documentation.
+
+    Args:
+        service (googleapiclient.discovery.Resource): The Google service resource.
+        resource (str): The name of the resource.
+        method (str): The name of the method.
+
+    Returns:
+        list: The names of the parameters for the API method, excluding non-parameter documentation.
+    """
+    resource_obj = getattr(service, resource)()
+    api_method = getattr(resource_obj, method)
+
+    parameter_names = []
+    if hasattr(api_method, "__doc__") and api_method.__doc__:
+        parsing_parameters = False
+        doc_lines = api_method.__doc__.splitlines()
+        for line in doc_lines:
+            if line.startswith("Args:"):
+                parsing_parameters = True
+            elif line.startswith("Returns:"):
+                parsing_parameters = False
+            elif parsing_parameters and ":" in line:
+                param, _ = line.split(":", 1)
+                parameter_names.append(param.strip())
+
+    return parameter_names
