61a7308ffae7b2ce2686465900c69d9af8db36d6
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2024-06-14 17:36:53 +0000

    test: create draft function

--- app/modules/dev/google_service.py
+++ app/modules/dev/google_service.py
@@ -1,9 +1,10 @@
 """Testing new google service (will be removed)"""
 
 import os
-from integrations.google_workspace import (
-    google_directory,
-)
+
+from integrations.google_workspace import gmail
+from integrations.slack import users as slack_users
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -29,10 +30,34 @@ def open_modal(client, body, folders):
     client.views_open(trigger_id=body["trigger_id"], view=view)
 
 
-def google_service_command(ack, client, body, respond):
+def google_service_command(ack, client, body, respond, logger):
     ack()
-    response = google_directory.list_groups()
+    post_content = test_content()
+    user_id = slack_users.get_user_email_from_body(client, body)
+    create_message = gmail.create_email_message(
+        "Test ATI message", post_content, user_id, ""
+    )
+
+    response = gmail.create_draft(
+        message=create_message,
+        user_id=user_id,
+        delegated_user_email=user_id,
+    )
+
+    logger.info(response)
     if not response:
         respond("No response")
     else:
-        respond(f"Found {len(response)} users")
+        respond("Found users")
+
+
+def test_content():
+    post_content = """
+        This is a test post content.
+        It can support multiple lines.
+
+        For now, it is just a simple string. Future versions will support more complex content.
+
+        Like HTML and markdown.
+    """
+    return post_content
