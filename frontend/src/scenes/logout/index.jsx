3aa3dadde3bf540f7472b1fb473a4b80986b64db
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2024-06-19 18:58:04 +0000

    Fixing up logout function

---
+++ frontend/src/scenes/logout/index.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Logout = () => {
+    const navigate = useNavigate();
+  
+    useEffect(() => {
+      const handleLogout = async () => {
+        try {
+          const response = await fetch(`${window.origin}/logout`, {
+            method: 'GET',
+            credentials: 'include', // Include credentials to manage session
+          });
+  
+          if (response.ok) {
+            // Clear the authentication state and redirect to the landing page
+            navigate('/');
+            window.location.reload(); // Refresh the session
+          } else {
+            console.error('Failed to log out');
+          }
+        } catch (error) {
+          console.error('An error occurred during logout:', error);
+        }
+      };
+  
+      handleLogout();
+    }, [navigate]);
+  
+    return null; // This component doesn't render anything
+  };
+  
+  export default Logout;
