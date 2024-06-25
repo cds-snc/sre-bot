35d02d886b632c89914fe0578c453d2ca627749e
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2024-06-25 17:00:38 +0000

    fix: setup test with new execute api call function

--- app/modules/dev/aws_dev.py
+++ app/modules/dev/aws_dev.py
@@ -1,7 +1,14 @@
 """Testing AWS service (will be removed)"""
 
+import datetime
+import json
 import logging
-from modules.aws import identity_center
+
+import boto3
+
+# from modules.aws import dynamodb
+from integrations.aws import dynamodb, identity_store, client as aws_client
+from integrations.slack import users as slack_users
 
 from dotenv import load_dotenv
 
@@ -12,10 +19,20 @@ logger = logging.getLogger(__name__)
 
 def aws_dev_command(ack, client, body, respond):
     ack()
-    response = identity_center.synchronize(
-        enable_user_create=False, enable_membership_create=False
+    account_id = "283582579564"
+    TableName = "aws_access_requests"
+    KeyConditionExpression = "account_id = :account_id and created_at > :created_at"
+    ExpressionAttributeValues = {
+        ":account_id": {"S": account_id},
+        ":created_at": {"N": str(datetime.datetime.now().timestamp() - (4 * 60 * 60))},
+    }
+    response = dynamodb.query(
+        table_name=TableName,
+        KeyConditionExpression=KeyConditionExpression,
+        ExpressionAttributeValues=ExpressionAttributeValues,
     )
     if not response:
         respond("Sync failed. See logs")
     else:
+        logger.info(response)
         respond("Sync successful.")
