9511c82b6dadcd9e9590ba5e5b5976dd5248fe7e
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2024-07-18 19:11:30 +0000

    fix: client config values

--- app/integrations/aws/dynamodb.py
+++ app/integrations/aws/dynamodb.py
@@ -2,7 +2,10 @@ import os
 from integrations.aws.client import (
     execute_aws_api_call,
     handle_aws_api_errors,
-    CLIENT_DEFAULTS as client_config,
+)
+
+client_config = dict(
+    region_name=os.environ.get("AWS_REGION", "ca-central-1"),
 )
 
 if os.environ.get("PREFIX", None):
@@ -11,12 +14,12 @@ if os.environ.get("PREFIX", None):
 
 @handle_aws_api_errors
 def query(
-    table_name, key_condition_expression=None, expression_attribute_values=None, **kwargs
+    table_name,
+    key_condition_expression=None,
+    expression_attribute_values=None,
+    **kwargs,
 ):
-    params = {
-        "TableName": table_name,
-        "client_config": client_config
-    }
+    params = {"TableName": table_name, "config": client_config}
     if key_condition_expression:
         params["KeyConditionExpression"] = key_condition_expression
     if expression_attribute_values:
@@ -31,7 +34,7 @@ def query(
 def scan(table, **kwargs):
     params = {
         "TableName": table,
-        "client_config": client_config,
+        "config": client_config,
     }
     if kwargs:
         params.update(kwargs)
