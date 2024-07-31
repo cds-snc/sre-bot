446c5c3b9908cef72fe30d59af07e7924be32a38
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2024-07-31 17:40:18 +0000

    feat: move security hub functions into integrations

---
+++ app/integrations/aws/security_hub.py
@@ -0,0 +1,24 @@
+import os
+from integrations.aws.client import execute_aws_api_call, handle_aws_api_errors
+
+LOGGING_ROLE_ARN = os.environ.get("AWS_LOGGING_ACCOUNT_ROLE_ARN")
+
+
+@handle_aws_api_errors
+def get_findings(filters):
+    """Retrieves all findings from AWS Security Hub
+
+    Args:
+        filters (dict): Filters to apply to the findings.
+
+    Returns:
+        list: A list of finding objects.
+    """
+    response = execute_aws_api_call(
+        "securityhub",
+        "get_findings",
+        paginated=True,
+        role_arn=LOGGING_ROLE_ARN,
+        filters=filters,
+    )
+    return response
