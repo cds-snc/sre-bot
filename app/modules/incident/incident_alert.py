72a0169709043749fc1c09430419bacfa481edbe
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2024-08-08 19:11:27 +0000

    feat: migrate alert related functions to own module

---
+++ app/modules/incident/incident_alert.py
@@ -0,0 +1,45 @@
+from modules import incident
+from models import webhooks
+
+
+def handle_incident_action_buttons(client, ack, body, logger):
+    delete_block = False
+    name = body["actions"][0]["name"]
+    value = body["actions"][0]["value"]
+    user = body["user"]["id"]
+    if name == "call-incident":
+        incident.open_modal(client, ack, {"text": value}, body)
+        incident.log_to_sentinel("call_incident_button_pressed", body)
+    elif name == "ignore-incident":
+        ack()
+        webhooks.increment_acknowledged_count(value)
+        attachments = body["original_message"]["attachments"]
+        msg = (
+            f"🙈  <@{user}> has acknowledged and ignored the incident.\n"
+            f"<@{user}> a pris connaissance et ignoré l'incident."
+        )
+        # if the last attachment is a preview from a link, switch the places of the last 2 attachments so that the incident buttons can be appended properly
+        if len(attachments) > 1:
+            if "app_unfurl_url" in attachments[-1]:
+                attachments[-2], attachments[-1] = attachments[-1], attachments[-2]
+        attachments[-1] = {
+            "color": "3AA3E3",
+            "fallback": f"{msg}",
+            "text": f"{msg}",
+        }
+        body["original_message"]["attachments"] = attachments
+        body["original_message"]["channel"] = body["channel"]["id"]
+
+        # rich_text blocks are only available for 1st party Slack clients (meaning Desktop, iOS, Android Slack apps)
+        # https://github.com/slackapi/bolt-js/issues/1324
+        if "blocks" in body["original_message"]:
+            for block in body["original_message"]["blocks"]:
+                if "type" in block and block["type"] == "rich_text":
+                    delete_block = True
+
+        if delete_block:
+            body["original_message"]["blocks"] = []
+
+        logger.info(f"Updating chat: {body['original_message']}")
+        client.api_call("chat.update", json=body["original_message"])
+        incident.log_to_sentinel("ignore_incident_button_pressed", body)
