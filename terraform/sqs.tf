7081bfaad717e36f6ede3ee57ac5e24cd4c0ec95
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2024-10-02 09:19:55 -0700

    Adding SQS to manage messages in the SRE bot (#666)
    * Adding SQS to manage messages in the SRE bot
    
    * Adding an additional comment for clarity

---
+++ terraform/sqs.tf
@@ -0,0 +1,22 @@
+# Create a FIFO SQS queue for the SRE Bot
+resource "aws_sqs_queue" "sre_bot_fifo_queue" {
+  name                        = "sre-bot-fifo-queue.fifo"
+  fifo_queue                  = true # Make sure that it is FIFO queue
+  content_based_deduplication = true # Enable content-based deduplication
+  delay_seconds               = 0    # Specify delay time for messages
+  visibility_timeout_seconds  = 30   # Specify the visibility timeout
+
+  depends_on = [aws_sqs_queue.sre_bot_dead_letter_queue]
+
+  # Specify dead-letter queue (DLQ) settings
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.sre_bot_dead_letter_queue.arn
+    maxReceiveCount     = 5 # Number of times a message is delivered before sending to DLQ
+  })
+}
+
+# Dead-letter queue resource, which is used to store messages that cannot be processed 
+resource "aws_sqs_queue" "sre_bot_dead_letter_queue" {
+  name       = "sre-bot-dead-letter-queue.fifo"
+  fifo_queue = true
+}
