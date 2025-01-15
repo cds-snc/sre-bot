70aef72cbc33508550b36fe7b648f64059696f3a
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2025-01-15 21:40:42 +0000

    feat: setup a Google Meet class to handle Hangouts

---
+++ app/integrations/google_next/meet.py
@@ -0,0 +1,47 @@
+from googleapiclient.discovery import Resource  # type: ignore
+from integrations.google_next.service import (
+    execute_google_api_call,
+    handle_google_api_errors,
+    get_google_service,
+    GOOGLE_DELEGATED_ADMIN_EMAIL,
+)
+
+
+class GoogleMeet:
+    """
+    A class to simplify the use of various Google Meet API operations across modules.
+
+    This class provides methods to interact with the Google Workspace Meet API, including creating new meetings, updating existing meetings, and retrieving meeting details, and includes error handling for Google API errors.
+
+    While this class aims to simplify the usage of the Google Meet API, it is always possible
+    to use the Google API Python client directly as per the official documentation:
+    (https://googleapis.github.io/google-api-python-client/docs/)
+
+    Attributes:
+        scopes (list): The list of scopes to request.
+        delegated_email (str): The email address of the user to impersonate.
+        service (Resource): Optional - An authenticated Google service resource. If provided, the service will be used instead of creating a new one.
+    """
+
+    def __init__(
+        self, scopes=None, delegated_email=None, service: Resource | None = None
+    ):
+        if not scopes and not service:
+            raise ValueError("Either scopes or a service must be provided.")
+        if not delegated_email and not service:
+            delegated_email = GOOGLE_DELEGATED_ADMIN_EMAIL
+        self.scopes = scopes
+        self.delegated_email = delegated_email
+        self.service = service if service else self._get_google_service()
+
+    def _get_google_service(self) -> Resource:
+        """Get authenticated directory service for Google Workspace."""
+        return get_google_service("meet", "v2", self.scopes, self.delegated_email)
+
+    @handle_google_api_errors
+    def create_space(self):
+        """Creates a new and empty space in Google Meet."""
+        config = {"accessType": "TRUSTED", "entryPointAccess": "ALL"}
+        return execute_google_api_call(
+            self.service, "spaces", "create", body={"config": config}
+        )
