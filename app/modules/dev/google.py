e44a1e84a4dc36875a247dc0ff5d17fc217af0b2
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2025-01-15 18:56:32 +0000

    test: performance between standalone fn and class implementation

--- app/modules/dev/google.py
+++ app/modules/dev/google.py
@@ -1,10 +1,12 @@
 """Testing new google service (will be removed)"""
 
+from datetime import datetime
 import os
 
 from integrations.google_workspace import (
     google_directory,
 )
+from integrations.google_next.directory import GoogleDirectory
 
 from dotenv import load_dotenv
 
@@ -22,16 +24,29 @@ def get_members(group):
 
 def google_service_command(ack, client, body, respond, logger):
     ack()
-    respond("Nothing to see here.")
-
-
-def test_content():
-    post_content = """
-        This is a test post content.
-        It can support multiple lines.
-
-        For now, it is just a simple string. Future versions will support more complex content.
-
-        Like HTML and markdown.
-    """
-    return post_content
+    scopes = [
+        "https://www.googleapis.com/auth/admin.directory.group.readonly",
+        "https://www.googleapis.com/auth/admin.directory.group.member.readonly",
+        "https://www.googleapis.com/auth/admin.directory.user.readonly",
+    ]
+    g_directory = GoogleDirectory(scopes=scopes)
+    query = "email:aws-*"
+    start_next = datetime.now()
+    groups_next = g_directory.list_groups_with_members(query=query)
+    end_next = datetime.now()
+    time_next = end_next - start_next
+    logger.info(f"Google Next API call took {time_next} to complete")
+    errors_next = any([group.get("error") for group in groups_next])
+    start = datetime.now()
+    groups = google_directory.list_groups_with_members(query=query)
+    end = datetime.now()
+    time = end - start
+    logger.info(f"Google Workspace API call took {time} to complete")
+    errors = any([group.get("error") for group in groups])
+    if groups and groups_next:
+        response_string = ""
+        response_string += f"Google Workspace API call took {time.total_seconds():.2f} to complete and found {len(groups)} groups (errors? {errors})\n"
+        response_string += f"Google Next API call took {time_next.total_seconds():.2f} to complete and found {len(groups_next)} groups (errors? {errors_next})"
+        respond(response_string)
+    else:
+        respond("Nothing to see here.")
