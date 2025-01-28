fad3167acfb937a32b8abd7ee133abdad717c3f1
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2025-01-28 21:32:29 +0000

    test: add stale channel test function

--- app/modules/dev/core.py
+++ app/modules/dev/core.py
@@ -19,3 +19,45 @@ def dev_command(ack, logger, respond, client, body, args):
             google.google_service_command(ack, client, body, respond, logger)
         case "slack":
             slack.slack_command(ack, client, body, respond, logger, args)
+        case "stale":
+            test_stale_channel_notification(ack, logger, respond, client, body)
+
+
+def test_stale_channel_notification(ack, logger, respond, client, body):
+    ack()
+    text = """👋  Hi! There have been no updates in this incident channel for 14 days! Consider scheduling a retro or archiving it.\n
+        Bonjour! Il n'y a pas eu de mise à jour dans ce canal d'incident depuis 14 jours. Pensez à planifier une rétro ou à l'archiver."""
+    attachments = [
+        {
+            "text": "Would you like to archive the channel now or schedule a retro? | Souhaitez-vous archiver le canal maintenant ou planifier une rétro?",
+            "fallback": "You are unable to archive the channel | Vous ne pouvez pas archiver ce canal",
+            "callback_id": "archive_channel",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+            "actions": [
+                {
+                    "name": "archive",
+                    "text": "Archive channel | Canal d'archives",
+                    "type": "button",
+                    "value": "archive",
+                    "style": "danger",
+                },
+                {
+                    "name": "schedule_retro",
+                    "text": "Schedule Retro | Calendrier rétro",
+                    "type": "button",
+                    "value": "schedule_retro",
+                    "style": "primary",
+                },
+                {
+                    "name": "ignore",
+                    "text": "Ignore | Ignorer",
+                    "type": "button",
+                    "value": "ignore",
+                },
+            ],
+        }
+    ]
+    client.chat_postMessage(
+        channel=body["channel_id"], text=text, attachments=attachments
+    )
