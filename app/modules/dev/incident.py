0187c7653d40d0b2cc2e9fb004462fe513ff2d58
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2025-02-07 16:21:03 +0000

    fix: limit number of incidents to fetch in dev

--- app/modules/dev/incident.py
+++ app/modules/dev/incident.py
@@ -11,6 +11,7 @@ def list_incidents(ack, logger, respond, client: WebClient, body):
     """List incidents"""
     logger.info("Listing incidents...")
     incidents = incident_folder.list_incidents()
+    respond(f"Found {len(incidents)} incidents")
     if len(incidents) > 10:
         incidents = incidents[:10]
     logger.info(json.dumps(incidents, indent=2))
@@ -20,14 +21,14 @@ def list_incidents(ack, logger, respond, client: WebClient, body):
             f"ID: {incident['id']['S']}\nName: {incident['name']['S']}\nStatus: {incident['status']['S']}\nEnvironment: {incident['environment']['S']}\nChannel: <#{incident['channel_id']['S']}>\n"
         )
     message = "\n\n".join(formatted_incidents)
-    respond(f"Found {len(incidents)} incidents:\n\n{message}")
+    respond(f"listing the 10 first incidents:\n\n{message}")
     logger.info("finished processing request")
 
 
 def load_incidents(ack, logger, respond, client: WebClient, body):
     """Load incidents from Google Sheet"""
     logger.info("Loading incidents...")
-    incidents = incident_folder.get_incidents_from_sheet()
+    incidents = incident_folder.get_incidents_from_sheet(15)
     incidents = incident_folder.complete_incidents_details(client, logger, incidents)
     count = incident_folder.create_missing_incidents(logger, incidents)
     respond(f"Loaded {count} incidents")
