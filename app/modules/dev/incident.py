64bb3b20ac1defc7bce82059b6dc23bb65646cc8
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2025-02-10 20:01:44 +0000

    feat: add function to handle incident creation from channel

--- app/modules/dev/incident.py
+++ app/modules/dev/incident.py
@@ -46,3 +46,48 @@ def load_incidents(ack, logger, respond, client: WebClient, body):
     respond(f"Created {count} new incidents")
     logger.info(f"{len(incidents)} incidents")
     logger.info("Finished loading incidents")
+
+
+def add_incident(ack, logger, respond, client: WebClient, body):
+    """Add an incident to local DB from a channel"""
+    is_incident, is_dev_incident = incident_conversation.is_incident_channel(
+        client, logger, body["channel_id"]
+    )
+    if not (is_incident and is_dev_incident):
+        respond("This is not an incident dev channel")
+        return
+    incident_id = incident_folder.get_incident_by_channel_id(body["channel_id"])
+    if incident_id:
+        respond(f"This channel is already associated with incident:\n{incident_id}")
+        return
+
+    incident_info = client.conversations_info(channel=body["channel_id"])
+    if not incident_info["ok"]:
+        respond("Error getting channel info")
+        return
+    channel = incident_info["channel"]
+    incident = {
+        "channel_id": channel["id"],
+        "channel_name": channel["name"],
+        "name": channel["topic"]["value"],
+        "user_id": body["user_id"],
+        "teams": ["Development"],
+        "created_at": channel["created"],
+    }
+    incident = incident_folder.get_incident_details(client, logger, incident)
+    if not incident.get("report_url"):
+        logger.info("Getting report url...")
+        incident["report_url"] = incident_conversation.get_incident_document_id(
+            client, incident["channel_id"], logger
+        )
+    incident_folder.create_incident(
+        incident["channel_id"],
+        incident["channel_name"],
+        incident["name"],
+        incident["user_id"],
+        incident["teams"],
+        incident["report_url"],
+        meet_url=incident["meet_url"],
+        created_at=incident["created_at"],
+    )
+    logger.info(incident)
