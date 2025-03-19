7566dd14c31d639950feb555016958df39c8caf7
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2025-03-19 16:34:08 +0000

    refactor: break submit function into small and reusable functions

--- app/modules/incident/schedule_retro.py
+++ app/modules/incident/schedule_retro.py
@@ -9,68 +9,43 @@ from integrations.google_workspace.google_calendar import (
     get_freebusy,
     insert_event,
     find_first_available_slot,
+    identify_unavailable_users,
 )
 from modules.incident import incident_conversation
 
 
 # Schedule a calendar event by finding the first available slot in the next 60 days that all participants are free in and book the event
-def schedule_event(event_details, days, user_emails):
+def schedule_event(days, user_emails, days_lookup=60):
     # Define the time range for the query
     now = datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0)
     # time_min is the current time + days and time_max is the current time + 60 days + days
     time_min = (now + timedelta(days=days)).isoformat() + "Z"
-    time_max = (now + timedelta(days=(60 + days))).isoformat() + "Z"
+    time_max = (now + timedelta(days=(days_lookup + days))).isoformat() + "Z"
 
     # Construct the items array
     items = []
     for email in user_emails:
         email = email.strip()
         items.append({"id": email})
-    incident_name = json.loads(event_details).get("name")
-
-    # get the incident document link
-    incident_document = json.loads(event_details).get("incident_document")
 
     # Execute the query to find all the busy times for all the participants
     freebusy_result = get_freebusy(time_min, time_max, items)
-
-    # return the first available slot to book the event
+    unavailable_users = identify_unavailable_users(freebusy_result, time_min, time_max)
+    if set(unavailable_users) == set(user_emails):
+        logging.warning(
+            "All users are unavailable for the next %s days starting from %s. Please check the user emails or the calendar settings.",
+            days_lookup,
+            time_min,
+        )
+    # # return the first available slot to book the event
     first_available_start, first_available_end = find_first_available_slot(
         freebusy_result, days
     )
-
-    # If there are no available slots, return None
-    if first_available_start is None and first_available_end is None:
-        logging.info("No available slots found")
-        return None
-
-    event_config = {
-        "description": "This is a retro meeting to discuss incident: " + incident_name,
-        "conferenceData": {
-            "createRequest": {
-                "requestId": f"{first_available_start.timestamp()}",  # Unique ID per event to avoid collisions
-                "conferenceSolutionKey": {
-                    "type": "hangoutsMeet"  # This automatically generates a Google Meet link
-                },
-            }
-        },
-        "reminders": {
-            "useDefault": False,
-            "overrides": [
-                {"method": "popup", "minutes": 10},
-            ],
-        },
+    return {
+        "first_available_start": first_available_start,
+        "first_available_end": first_available_end,
+        "unavailable_users": unavailable_users,
     }
-
-    result = insert_event(
-        first_available_start.isoformat(),
-        first_available_end.isoformat(),
-        user_emails,
-        "Retro " + incident_name,
-        incident_document,
-        **event_config,
-    )
-    return result  # Return the HTML link and event info
 
 
 # Function to be triggered when the /sre incident schedule command is called. This function brings up a modal window
@@ -209,34 +184,84 @@ def generate_retro_options_view(
         "private_metadata": private_metadata,
         "title": {"type": "plain_text", "text": "SRE - Schedule Retro 🗓️"},
         "submit": {"type": "plain_text", "text": "Schedule"},
-        "submit_disabled": False,
         "blocks": all_blocks,
     }
     # Return the completed modal
     return view
 
 
-# Function to create the calendar event and bring up a modal that contains a link to the event. If the event could not be scheduled,
-# a message is displayed to the user that the event could not be scheduled.
-def save_incident_retro(client: WebClient, ack, body, view):
-    ack()
-    blocks = {
+def generate_retro_saving_view(status=0, link=""):
+    """Create the modal for the saving retro event. Shows a loading message or success message with link.
+    Status 0: Loading message
+    Status 1: Success message with link
+    Status 2: Error message
+
+    :param status: int: Status of the view to generate
+    :param link: str: Link to the calendar event
+    :return: dict: View for the modal
+    """
+    if status == 0:
+        blocks = [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": ":beach-ball: *Scheduling the retro...*",
+                },
+            }
+        ]
+    elif status == 1:
+        blocks = [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "*Successfully scheduled calender event!*",
+                },
+                "accessory": {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "View Calendar Event",
+                    },
+                    "value": "view_event",
+                    "url": link,
+                    "action_id": "confirm_click",
+                },
+            },
+        ]
+    else:
+        blocks = [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "*Could not schedule event - no free time was found!*",
+                },
+            }
+        ]
+
+    view = {
         "type": "modal",
         "title": {"type": "plain_text", "text": "SRE - Schedule Retro 🗓️"},
-        "blocks": (
-            [
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": ":beach-ball: *Scheduling the retro...*",
-                    },
-                }
-            ]
-        ),
+        "blocks": blocks,
     }
-    loading_view = client.views_open(trigger_id=body["trigger_id"], view=blocks)["view"]
+    return view
 
+
+# Function to create the calendar event and bring up a modal that contains a link to the event. If the event could not be scheduled,
+# a message is displayed to the user that the event could not be scheduled.
+def handle_schedule_retro_submit(client: WebClient, ack, body, view):
+    """Handle the submission of the retro event modal."""
+    ack()
+    saving_view = generate_retro_saving_view()
+    loading_view = client.views_open(trigger_id=body["trigger_id"], view=saving_view)[
+        "view"
+    ]
+
+    private_metadata = json.loads(view["private_metadata"])
+    incident_name = private_metadata.get("name")
+    incident_document = private_metadata.get("incident_document")
     # get the number of days data from the view and convert to an integer
     days = int(view["state"]["values"]["number_of_days"]["number_of_days"]["value"])
 
@@ -251,24 +276,29 @@ def save_incident_retro(client: WebClient, ack, body, view):
         user_emails.append(user_email)
 
     # pass the data using the view["private_metadata"] to the schedule_event function
-    result = schedule_event(view["private_metadata"], days, user_emails)
+    scheduled_event_details = schedule_event(days, user_emails)
+    first_available_start = scheduled_event_details["first_available_start"]
+    first_available_end = scheduled_event_details["first_available_end"]
+    unavailable_users = scheduled_event_details["unavailable_users"]
+    if first_available_start is None:
+        saving_view = generate_retro_saving_view(status=2)
+        logging.info(
+            "The event could not be scheduled since no free time was found for the next 60 days"
+        )
+        client.views_update(
+            view_id=loading_view["id"], hash=loading_view["hash"], view=saving_view
+        )
+        return
+    result = save_retro_event(
+        first_available_start,
+        first_available_end,
+        user_emails,
+        incident_name,
+        incident_document,
+    )
     # if we could not schedule the event, display a message to the user that the event could not be scheduled
     if result is None:
-        blocks = {
-            "type": "modal",
-            "title": {"type": "plain_text", "text": "SRE - Schedule Retro 🗓️"},
-            "blocks": (
-                [
-                    {
-                        "type": "section",
-                        "text": {
-                            "type": "mrkdwn",
-                            "text": "*Could not schedule event - no free time was found!*",
-                        },
-                    }
-                ]
-            ),
-        }
+        saving_view = generate_retro_saving_view(status=2)
         logging.info(
             "The event could not be scheduled since schedule_event returned None"
         )
@@ -278,31 +308,7 @@ def save_incident_retro(client: WebClient, ack, body, view):
         channel_id = json.loads(view["private_metadata"])["channel_id"]
         event_link = result["event_link"]
         event_info = result["event_info"]
-        blocks = {
-            "type": "modal",
-            "title": {"type": "plain_text", "text": "SRE - Schedule Retro 🗓️"},
-            "blocks": (
-                [
-                    {
-                        "type": "section",
-                        "text": {
-                            "type": "mrkdwn",
-                            "text": "*Successfully scheduled calender event!*",
-                        },
-                        "accessory": {
-                            "type": "button",
-                            "text": {
-                                "type": "plain_text",
-                                "text": "View Calendar Event",
-                            },
-                            "value": "view_event",
-                            "url": f"{event_link}",
-                            "action_id": "confirm_click",
-                        },
-                    },
-                ]
-            ),
-        }
+        saving_view = generate_retro_saving_view(status=1, link=event_link)
         logging.info("Event has been scheduled successfully. Link: %s", event_link)
 
         # post the message in the channel
@@ -310,7 +316,41 @@ def save_incident_retro(client: WebClient, ack, body, view):
 
     # Open the modal and log that the event was scheduled successfully
     client.views_update(
-        view_id=loading_view["id"], hash=loading_view["hash"], view=blocks
+        view_id=loading_view["id"], hash=loading_view["hash"], view=saving_view
+    )
+
+
+def save_retro_event(
+    first_available_start,
+    first_available_end,
+    user_emails,
+    incident_name,
+    incident_document,
+):
+    event_config = {
+        "description": "This is a retro meeting to discuss incident: " + incident_name,
+        "conferenceData": {
+            "createRequest": {
+                "requestId": f"{first_available_start.timestamp()}",  # Unique ID per event to avoid collisions
+                "conferenceSolutionKey": {
+                    "type": "hangoutsMeet"  # This automatically generates a Google Meet link
+                },
+            }
+        },
+        "reminders": {
+            "useDefault": False,
+            "overrides": [
+                {"method": "popup", "minutes": 10},
+            ],
+        },
+    }
+    return insert_event(
+        first_available_start.isoformat(),
+        first_available_end.isoformat(),
+        user_emails,
+        "Retro " + incident_name,
+        incident_document,
+        **event_config,
     )
 
 
