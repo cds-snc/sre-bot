d2e502d0e17c22848e5e8eb5a71a9236e6ff9334
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2025-03-26 17:18:03 +0000

    refactor(notify): export functions

--- app/integrations/notify/__init__.py
+++ app/integrations/notify/__init__.py
@@ -1,1 +1,17 @@
-from .
+"""Notify module for sending events to the Notify API."""
+
+from .client import (
+    epoch_seconds,
+    create_jwt_token,
+    create_authorization_header,
+    post_event,
+    revoke_api_key,
+)
+
+__all__ = [
+    "epoch_seconds",
+    "create_jwt_token",
+    "create_authorization_header",
+    "post_event",
+    "revoke_api_key",
+]
