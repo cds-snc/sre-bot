dee8c168f3209d1a5d722428edc32ab9fc9065d3
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2025-04-10 22:07:12 +0000

    feat: add UserProvider context for user authentication management

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,15 +2,17 @@
  * Entry point of the React application.
  * Renders the <App /> component inside the root element of the HTML document.
  */
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { UserProvider } from "./context/UserProvider";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <UserProvider>
+      <App />
+    </UserProvider>
   </React.StrictMode>
 );
