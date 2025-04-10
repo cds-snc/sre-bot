6a813fd1c43613949042fb9808e9776228f2ceb9
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2025-04-10 23:40:14 +0000

    feat: implement webhook handling and testing for various payload types

---
+++ app/api/v1/routes/webhooks.py
@@ -0,0 +1,198 @@
+import requests  # type: ignore
+import json
+from fastapi import APIRouter, Request, HTTPException
+from models.webhooks import AwsSnsPayload, WebhookPayload
+from core.logging import get_module_logger
+from api.dependencies.rate_limits import get_limiter
+from integrations.sentinel import log_to_sentinel
+from modules.slack import webhooks
+from server.event_handlers import aws
+from server.utils import (
+    log_ops_message,
+)
+
+
+logger = get_module_logger()
+router = APIRouter(tags=["Access"])
+limiter = get_limiter()
+
+
+@router.post("/hook/{id}")
+@limiter.limit(
+    "30/minute"
+)  # since some slack channels use this for alerting, we want to be generous with the rate limiting on this one
+def handle_webhook(
+    id: str,
+    payload: WebhookPayload | str,
+    request: Request,
+):
+    webhook = webhooks.get_webhook(id)
+    webhook_payload = WebhookPayload()
+    if webhook:
+        hook_type: str = webhook.get("hook_type", {"S": "alert"})["S"]
+        # if the webhook is active, then send forward the response to the webhook
+        if webhooks.is_active(id):
+            webhooks.increment_invocation_count(id)
+            if isinstance(payload, str):
+                processed_payload = handle_string_payload(payload, request)
+                if isinstance(processed_payload, dict):
+                    return processed_payload
+                else:
+                    logger.info(
+                        "payload_processed",
+                        payload=processed_payload,
+                        webhook_id=id,
+                    )
+                    webhook_payload = processed_payload
+            else:
+                webhook_payload = payload
+            webhook_payload.channel = webhook["channel"]["S"]
+            if hook_type == "alert":
+                webhook_payload = append_incident_buttons(webhook_payload, id)
+            try:
+                webhook_payload_parsed = webhook_payload.model_dump(exclude_none=True)
+                request.state.bot.client.api_call(
+                    "chat.postMessage", json=webhook_payload_parsed
+                )
+                log_to_sentinel(
+                    "webhook_sent",
+                    {"webhook": webhook, "payload": webhook_payload_parsed},
+                )
+                return {"ok": True}
+            except Exception as e:
+                logger.exception(
+                    "webhook_posting_error",
+                    webhook_id=id,
+                    webhook_payload=webhook_payload,
+                    error=str(e),
+                )
+                body = webhook_payload.model_dump(exclude_none=True)
+                log_ops_message(
+                    request.state.bot.client, f"Error posting message: ```{body}```"
+                )
+                raise HTTPException(
+                    status_code=500, detail="Failed to send message"
+                ) from e
+        else:
+            logger.info(
+                "webhook_not_active",
+                webhook_id=id,
+                webhook_payload=webhook_payload,
+                error="Webhook is not active",
+            )
+            raise HTTPException(status_code=404, detail="Webhook not active")
+    else:
+        raise HTTPException(status_code=404, detail="Webhook not found")
+
+
+def handle_string_payload(
+    payload: str,
+    request: Request,
+) -> WebhookPayload | dict:
+
+    string_payload_type, validated_payload = webhooks.validate_string_payload_type(
+        payload
+    )
+    logger.info(
+        "string_payload_type",
+        payload=payload,
+        string_payload_type=string_payload_type,
+        validated_payload=validated_payload,
+    )
+    match string_payload_type:
+        case "WebhookPayload":
+            webhook_payload = WebhookPayload(**validated_payload)
+        case "AwsSnsPayload":
+            awsSnsPayload = aws.validate_sns_payload(
+                AwsSnsPayload(**validated_payload),
+                request.state.bot.client,
+            )
+            if awsSnsPayload.Type == "SubscriptionConfirmation":
+                requests.get(awsSnsPayload.SubscribeURL, timeout=60)
+                logger.info(
+                    "subscribed_webhook_to_topic",
+                    webhook_id=awsSnsPayload.TopicArn,
+                    subscribed_topic=awsSnsPayload.TopicArn,
+                )
+                log_ops_message(
+                    request.state.bot.client,
+                    f"Subscribed webhook {id} to topic {awsSnsPayload.TopicArn}",
+                )
+                return {"ok": True}
+            if awsSnsPayload.Type == "UnsubscribeConfirmation":
+                log_ops_message(
+                    request.state.bot.client,
+                    f"{awsSnsPayload.TopicArn} unsubscribed from webhook {id}",
+                )
+                return {"ok": True}
+            if awsSnsPayload.Type == "Notification":
+                blocks = aws.parse(awsSnsPayload, request.state.bot.client)
+                # if we have an empty message, log that we have an empty
+                # message and return without posting to slack
+                if not blocks:
+                    logger.info(
+                        "payload_empty_message",
+                    )
+                    return {"ok": True}
+                webhook_payload = WebhookPayload(blocks=blocks)
+        case "AccessRequest":
+            # Temporary fix for the Access Request payloads
+            message = json.dumps(validated_payload)
+            webhook_payload = WebhookPayload(text=message)
+        case "UpptimePayload":
+            # Temporary fix for Upptime payloads
+            text = validated_payload.get("text", "")
+            header_text = "📈 Web Application Status Changed!"
+            blocks = [
+                {"type": "section", "text": {"type": "mrkdwn", "text": " "}},
+                {
+                    "type": "header",
+                    "text": {"type": "plain_text", "text": f"{header_text}"},
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": f"{text}",
+                    },
+                },
+            ]
+            webhook_payload = WebhookPayload(blocks=blocks)
+        case _:
+            raise HTTPException(
+                status_code=500,
+                detail="Invalid payload type. Must be a WebhookPayload object or a recognized string payload type.",
+            )
+    return WebhookPayload(**webhook_payload.model_dump(exclude_none=True))
+
+
+def append_incident_buttons(payload: WebhookPayload, webhook_id):
+    if payload.attachments is None:
+        payload.attachments = []
+    elif isinstance(payload.attachments, str):
+        payload.attachments = [payload.attachments]
+    payload.attachments += [
+        {
+            "fallback": "Incident",
+            "callback_id": "handle_incident_action_buttons",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+            "actions": [
+                {
+                    "name": "call-incident",
+                    "text": "🎉   Call incident ",
+                    "type": "button",
+                    "value": payload.text,
+                    "style": "primary",
+                },
+                {
+                    "name": "ignore-incident",
+                    "text": "🙈   Acknowledge and ignore",
+                    "type": "button",
+                    "value": webhook_id,
+                    "style": "default",
+                },
+            ],
+        }
+    ]
+    return payload
---
+++ app/tests/api/v1/test_webhooks.py
@@ -0,0 +1,439 @@
+from unittest.mock import patch, MagicMock, PropertyMock, call
+import pytest
+from fastapi import HTTPException
+from fastapi.testclient import TestClient
+from api.v1.routes import webhooks
+from models.webhooks import AwsSnsPayload
+from utils.tests import create_test_app
+from server import bot_middleware
+
+middlewares = [(bot_middleware.BotMiddleware, {"bot": MagicMock()})]
+test_app = create_test_app(webhooks.router, middlewares)
+client = TestClient(test_app)
+
+
+@patch("api.v1.routes.webhooks.append_incident_buttons")
+@patch("api.v1.routes.webhooks.webhooks.get_webhook")
+@patch("api.v1.routes.webhooks.webhooks.is_active")
+@patch("api.v1.routes.webhooks.webhooks.increment_invocation_count")
+@patch("api.v1.routes.webhooks.log_to_sentinel")
+def test_handle_webhook_found(
+    _log_to_sentinel_mock,
+    increment_invocation_count_mock,
+    is_active_mock,
+    get_webhook_mock,
+    append_incident_buttons_mock,
+):
+    get_webhook_mock.return_value = {"channel": {"S": "channel"}}
+    is_active_mock.return_value = True
+    payload = {"channel": "channel"}
+    append_incident_buttons_mock.return_value.json.return_value = "[]"
+    response = client.post("/hook/id", json=payload)
+    assert response.status_code == 200
+    assert response.json() == {"ok": True}
+    assert get_webhook_mock.call_count == 1
+    assert increment_invocation_count_mock.call_count == 1
+    assert append_incident_buttons_mock.call_count == 1
+
+
+@patch("api.v1.routes.webhooks.webhooks.get_webhook")
+def test_handle_webhook_not_found(get_webhook_mock):
+    get_webhook_mock.return_value = None
+    payload = {"channel": "channel"}
+    response = client.post("/hook/id", json=payload)
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Webhook not found"}
+    assert get_webhook_mock.call_count == 1
+
+
+@patch("api.v1.routes.webhooks.append_incident_buttons")
+@patch("api.v1.routes.webhooks.webhooks.get_webhook")
+@patch("api.v1.routes.webhooks.webhooks.is_active")
+@patch("api.v1.routes.webhooks.webhooks.increment_invocation_count")
+@patch("api.v1.routes.webhooks.log_to_sentinel")
+def test_handle_webhook_disabled(
+    _log_to_sentinel_mock,
+    increment_invocation_count_mock,
+    is_active_mock,
+    get_webhook_mock,
+    append_incident_buttons_mock,
+):
+    is_active_mock.return_value = False
+    get_webhook_mock.return_value = {"channel": {"S": "channel"}}
+    payload = {"channel": "channel"}
+    append_incident_buttons_mock.return_value.json.return_value = "[]"
+    response = client.post("/hook/id", json=payload)
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Webhook not active"}
+    assert get_webhook_mock.call_count == 1
+    assert increment_invocation_count_mock.call_count == 0
+    assert append_incident_buttons_mock.call_count == 0
+
+
+@patch("api.v1.routes.webhooks.append_incident_buttons")
+@patch("api.v1.routes.webhooks.webhooks.get_webhook")
+@patch("api.v1.routes.webhooks.webhooks.is_active")
+@patch("api.v1.routes.webhooks.webhooks.increment_invocation_count")
+@patch("api.v1.routes.webhooks.log_ops_message")
+def test_handle_webhook_found_but_exception(
+    log_ops_message_mock,
+    increment_invocation_count_mock,
+    is_active_mock,
+    get_webhook_mock,
+    append_incident_buttons_mock,
+):
+    get_webhook_mock.return_value = {"channel": {"S": "channel"}}
+    is_active_mock.return_value = True
+    payload = MagicMock()
+    append_incident_buttons_mock.return_value.json.return_value = "[]"
+    request = MagicMock()
+    request.state.bot.client.api_call.side_effect = Exception("error")
+    with pytest.raises(Exception):
+        webhooks.handle_webhook("id", payload, request)
+        assert log_ops_message_mock.call_count == 1
+
+
+@patch("api.v1.routes.webhooks.webhooks.get_webhook")
+@patch("api.v1.routes.webhooks.webhooks.is_active")
+@patch("api.v1.routes.webhooks.webhooks.increment_invocation_count")
+@patch("api.v1.routes.webhooks.log_ops_message")
+@patch("api.v1.routes.webhooks.handle_string_payload")
+def test_handle_webhook_string_returns_webhook_payload(
+    handle_string_payload_mock,
+    _log_ops_message_mock,
+    _increment_invocation_count_mock,
+    is_active_mock,
+    get_webhook_mock,
+    caplog,
+):
+    get_webhook_mock.return_value = {"channel": {"S": "channel"}}
+    is_active_mock.return_value = True
+    payload = '{"channel": "channel"}'
+    handle_string_payload_mock.return_value = {"channel": "channel", "blocks": "blocks"}
+    response = client.post("/hook/id", json=payload)
+    assert response.status_code == 200
+    assert response.json() == {"channel": "channel", "blocks": "blocks"}
+    assert handle_string_payload_mock.call_count == 1
+
+
+@patch("api.v1.routes.webhooks.webhooks.get_webhook")
+@patch("api.v1.routes.webhooks.webhooks.is_active")
+@patch("api.v1.routes.webhooks.webhooks.increment_invocation_count")
+@patch("api.v1.routes.webhooks.log_ops_message")
+@patch("api.v1.routes.webhooks.handle_string_payload")
+def test_handle_webhook_string_payload_returns_OK_status(
+    handle_string_payload_mock,
+    _log_ops_message_mock,
+    _increment_invocation_count_mock,
+    is_active_mock,
+    get_webhook_mock,
+):
+    get_webhook_mock.return_value = {"channel": {"S": "channel"}}
+    is_active_mock.return_value = True
+    payload = "test"
+    handle_string_payload_mock.return_value = {"ok": True}
+    response = client.post("/hook/id", json=payload)
+    assert response.status_code == 200
+    assert response.json() == {"ok": True}
+    assert handle_string_payload_mock.call_count == 1
+
+
+@patch("api.v1.routes.webhooks.webhooks.validate_string_payload_type")
+def test_handle_string_payload_with_webhook_string(
+    validate_string_payload_type_mock,
+):
+    request = MagicMock()
+    validate_string_payload_type_mock.return_value = (
+        "WebhookPayload",
+        {"channel": "channel"},
+    )
+    payload = '{"channel": "channel"}'
+    response = webhooks.handle_string_payload(payload, request)
+    assert response.channel == "channel"
+
+
+@patch("api.v1.routes.webhooks.aws.parse")
+@patch("api.v1.routes.webhooks.aws.validate_sns_payload")
+@patch("api.v1.routes.webhooks.webhooks.validate_string_payload_type")
+def test_handle_string_payload_with_aws_sns_notification_without_message(
+    validate_string_payload_type_mock,
+    validate_sns_payload_mock,
+    parse_mock,
+):
+    request = MagicMock()
+    payload = '{"Type": "Notification", "Message": "{}"}'
+    validate_string_payload_type_mock.return_value = (
+        "AwsSnsPayload",
+        {"Type": "Notification", "Message": ""},
+    )
+    validate_sns_payload_mock.return_value = AwsSnsPayload(
+        Type="Notification", Message=""
+    )
+    parse_mock.return_value = ""
+    response = webhooks.handle_string_payload(payload, request)
+    assert response == {"ok": True}
+
+
+@patch("api.v1.routes.webhooks.aws.parse")
+@patch("api.v1.routes.webhooks.aws.validate_sns_payload")
+@patch("api.v1.routes.webhooks.webhooks.validate_string_payload_type")
+def test_handle_string_payload_with_aws_sns_notification(
+    validate_string_payload_type_mock, validate_sns_payload_mock, parse_mock
+):
+    request = MagicMock()
+    validate_string_payload_type_mock.return_value = (
+        "AwsSnsPayload",
+        {"Type": "Notification", "Message": "message"},
+    )
+    payload = '{"Type": "Notification", "Message": "message"}'
+    validate_sns_payload_mock.return_value = AwsSnsPayload(
+        Type="Notification", Message="message"
+    )
+    parse_mock.return_value = "parsed_blocks"
+    response = webhooks.handle_string_payload(payload, request)
+    assert response.blocks == "parsed_blocks"
+
+
+@patch("api.v1.routes.webhooks.log_ops_message")
+@patch("api.v1.routes.webhooks.requests.get")
+@patch("api.v1.routes.webhooks.aws.validate_sns_payload")
+@patch("api.v1.routes.webhooks.webhooks.validate_string_payload_type")
+def test_handle_string_payload_with_aws_sns_subscription_confirmation(
+    validate_string_payload_type_mock,
+    validate_sns_payload_mock,
+    get_mock,
+    log_ops_message_mock,
+):
+    request = MagicMock()
+    payload = (
+        '{"Type": "SubscriptionConfirmation", "SubscribeURL": "http://example.com"}'
+    )
+    validate_string_payload_type_mock.return_value = (
+        "AwsSnsPayload",
+        {"Type": "SubscriptionConfirmation", "SubscribeURL": "http://example.com"},
+    )
+    validate_sns_payload_mock.return_value = AwsSnsPayload(
+        Type="SubscriptionConfirmation", SubscribeURL="http://example.com"
+    )
+    response = webhooks.handle_string_payload(payload, request)
+    assert response == {"ok": True}
+    assert log_ops_message_mock.call_count == 1
+
+
+@patch("api.v1.routes.webhooks.aws.validate_sns_payload")
+@patch("api.v1.routes.webhooks.webhooks.validate_string_payload_type")
+def test_handle_string_payload_with_aws_sns_unsubscribe_confirmation(
+    validate_string_payload_type_mock, validate_sns_payload_mock
+):
+    request = MagicMock()
+    validate_string_payload_type_mock.return_value = (
+        "AwsSnsPayload",
+        {
+            "Type": "UnsubscribeConfirmation",
+            "TopicArn": "arn:aws:sns:us-east-1:123456789012:MyTopic",
+        },
+    )
+    payload = '{"Type": "UnsubscribeConfirmation", "TopicArn": "arn:aws:sns:us-east-1:123456789012:MyTopic"}'
+    validate_sns_payload_mock.return_value = AwsSnsPayload(
+        Type="UnsubscribeConfirmation",
+        TopicArn="arn:aws:sns:us-east-1:123456789012:MyTopic",
+    )
+    response = webhooks.handle_string_payload(payload, request)
+    assert response == {"ok": True}
+
+
+@patch("api.v1.routes.webhooks.webhooks.validate_string_payload_type")
+def test_handle_string_payload_with_access_request(validate_string_payload_type_mock):
+    request = MagicMock()
+    validate_string_payload_type_mock.return_value = (
+        "AccessRequest",
+        {"user": "user1"},
+    )
+    payload = '{"user": "user1"}'
+    response = webhooks.handle_string_payload(payload, request)
+    assert response.text == '{"user": "user1"}'
+
+
+@patch("api.v1.routes.webhooks.webhooks.validate_string_payload_type")
+def test_handle_string_payload_with_upptime_payload(validate_string_payload_type_mock):
+    request = MagicMock()
+    payload = '{"text": "🟥 Payload Test (https://not-valid.cdssandbox.xyz/) is **down** : https://github.com/cds-snc/status-statut/issues/222"}'
+    validate_string_payload_type_mock.return_value = (
+        "UpptimePayload",
+        {
+            "text": "🟥 Payload Test (https://not-valid.cdssandbox.xyz/) is **down** : https://github.com/cds-snc/status-statut/issues/222"
+        },
+    )
+    response = webhooks.handle_string_payload(payload, request)
+    assert response.blocks == [
+        {"text": {"text": " ", "type": "mrkdwn"}, "type": "section"},
+        {
+            "text": {
+                "text": "📈 Web Application Status Changed!",
+                "type": "plain_text",
+            },
+            "type": "header",
+        },
+        {
+            "text": {
+                "text": "🟥 Payload Test (https://not-valid.cdssandbox.xyz/) is **down** : https://github.com/cds-snc/status-statut/issues/222",
+                "type": "mrkdwn",
+            },
+            "type": "section",
+        },
+    ]
+
+
+@patch("api.v1.routes.webhooks.webhooks.validate_string_payload_type")
+def test_handle_string_payload_with_invalid_payload_type(
+    validate_string_payload_type_mock,
+):
+    request = MagicMock()
+    validate_string_payload_type_mock.return_value = (
+        "InvalidPayloadType",
+        {},
+    )
+    payload = "{}"
+    with pytest.raises(HTTPException) as exc_info:
+        webhooks.handle_string_payload(payload, request)
+    assert exc_info.value.status_code == 500
+    assert (
+        exc_info.value.detail
+        == "Invalid payload type. Must be a WebhookPayload object or a recognized string payload type."
+    )
+
+
+@patch("api.v1.routes.webhooks.webhooks.get_webhook")
+@patch("api.v1.routes.webhooks.webhooks.is_active")
+@patch("api.v1.routes.webhooks.webhooks.increment_invocation_count")
+@patch("api.v1.routes.webhooks.log_ops_message")
+def test_handle_string_payload_with_invalid_json_payload(
+    _log_ops_message_mock,
+    _increment_invocation_count_mock,
+    is_active_mock,
+    get_webhook_mock,
+):
+    get_webhook_mock.return_value = {"channel": {"S": "channel"}}
+    is_active_mock.return_value = True
+    payload = "not a json payload"
+    response = client.post("/hook/id", json=payload)
+    assert response.status_code == 500
+    assert response.json() == {
+        "detail": "Invalid payload type. Must be a WebhookPayload object or a recognized string payload type."
+    }
+
+
+def test_handle_string_payload_with_valid_json_payload():
+    pass
+
+
+def test_append_incident_buttons_with_list_attachments():
+    payload = MagicMock()
+    attachments = PropertyMock(return_value=[])
+    type(payload).attachments = attachments
+    type(payload).text = PropertyMock(return_value="text")
+    webhook_id = "bar"
+    resp = webhooks.append_incident_buttons(payload, webhook_id)
+    assert payload == resp
+    assert attachments.call_count == 4
+    assert attachments.call_args_list == [
+        call(),
+        call(),
+        call(),
+        call(
+            [
+                {
+                    "fallback": "Incident",
+                    "callback_id": "handle_incident_action_buttons",
+                    "color": "#3AA3E3",
+                    "attachment_type": "default",
+                    "actions": [
+                        {
+                            "name": "call-incident",
+                            "text": "🎉   Call incident ",
+                            "type": "button",
+                            "value": "text",
+                            "style": "primary",
+                        },
+                        {
+                            "name": "ignore-incident",
+                            "text": "🙈   Acknowledge and ignore",
+                            "type": "button",
+                            "value": "bar",
+                            "style": "default",
+                        },
+                    ],
+                }
+            ]
+        ),
+    ]
+
+
+def test_append_incident_buttons_with_none_attachments():
+    payload = MagicMock()
+    payload.attachments = None
+    payload.text = "text"
+    webhook_id = "bar"
+
+    resp = webhooks.append_incident_buttons(payload, webhook_id)
+
+    assert payload == resp
+    assert payload.attachments == [
+        {
+            "fallback": "Incident",
+            "callback_id": "handle_incident_action_buttons",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+            "actions": [
+                {
+                    "name": "call-incident",
+                    "text": "🎉   Call incident ",
+                    "type": "button",
+                    "value": "text",
+                    "style": "primary",
+                },
+                {
+                    "name": "ignore-incident",
+                    "text": "🙈   Acknowledge and ignore",
+                    "type": "button",
+                    "value": "bar",
+                    "style": "default",
+                },
+            ],
+        }
+    ]
+
+
+def test_append_incident_buttons_with_str_attachments():
+    payload = MagicMock()
+    payload.attachments = "existing_attachment"
+    payload.text = "text"
+    webhook_id = "bar"
+
+    resp = webhooks.append_incident_buttons(payload, webhook_id)
+    assert payload == resp
+    assert payload.attachments == [
+        "existing_attachment",
+        {
+            "fallback": "Incident",
+            "callback_id": "handle_incident_action_buttons",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+            "actions": [
+                {
+                    "name": "call-incident",
+                    "text": "🎉   Call incident ",
+                    "type": "button",
+                    "value": "text",
+                    "style": "primary",
+                },
+                {
+                    "name": "ignore-incident",
+                    "text": "🙈   Acknowledge and ignore",
+                    "type": "button",
+                    "value": "bar",
+                    "style": "default",
+                },
+            ],
+        },
+    ]
