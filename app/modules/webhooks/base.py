00d3f841f903d18754807510c81aae5ca70c64ee
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2025-09-25 19:52:34 +0000

    refactor: simplify validate_payload function

--- app/modules/webhooks/base.py
+++ app/modules/webhooks/base.py
@@ -1,4 +1,3 @@
-import json
 from typing import Any, Dict, List, Optional, Tuple, Type
 
 from pydantic import BaseModel
@@ -8,49 +7,26 @@ from models.webhooks import WebhookPayload, AwsSnsPayload, AccessRequest, Upptim
 
 logger = get_module_logger()
 
-MODELS = [WebhookPayload, AwsSnsPayload, AccessRequest, UpptimePayload]
-
 
 def validate_payload(
-    payload: str | dict,
-    models: List[Type[BaseModel]],
+    payload_dict: dict,
     priorities: Optional[Dict[Type[BaseModel], int]] = None,
-) -> Optional[Tuple[type[BaseModel], Any]]:
+) -> Optional[Tuple[Type[BaseModel], Any]]:
     """
-    Validate the incoming webhook payload against the known models.
+    Wrapper around select_best_model to validate incoming webhook payloads.
 
     Args:
-        payload (str | dict): The incoming webhook payload, either as a JSON string or a dictionary.
-        models (List[Type[BaseModel]]): The list of known models to validate against.
+        payload (dict): The incoming webhook payload, either as a JSON string or a dictionary.
         priorities (Optional[Dict[Type[BaseModel], int]]): Optional dictionary of model priorities.
 
     Returns:
         Optional[BaseModel]: The validated payload as a Pydantic model, or None if validation fails.
     """
-    if not models:
-        logger.warning(
-            "payload_validation_error", error="No models provided for validation"
-        )
-        return None
-    payload_dict = None
-    result = None
-    # handle payload input types
-    if isinstance(payload, str):
-        try:
-            payload_dict = json.loads(payload)
-        except json.JSONDecodeError:
-            logger.warning("payload_validation_error", error="Invalid JSON payload")
-            return None
-    elif isinstance(payload, dict):
-        payload_dict = payload
-    else:
-        logger.warning(
-            "payload_validation_error",
-            error="Unsupported payload type",
-            payload_type=type(payload).__name__,
-        )
-        return None
+    models: List[Type[BaseModel]] = [
+        WebhookPayload,
+        AwsSnsPayload,
+        AccessRequest,
+        UpptimePayload,
+    ]
 
-    if payload_dict is not None:
-        result = select_best_model(payload_dict, models, priorities)
-    return result
+    return select_best_model(payload_dict, models, priorities)
--- app/tests/modules/webhooks/test_webhooks_base.py
+++ app/tests/modules/webhooks/test_webhooks_base.py
@@ -1,94 +1,25 @@
-import json
-from typing import Optional
-from unittest.mock import patch
-
+from unittest.mock import patch, ANY
 from modules.webhooks import base
-from pydantic import BaseModel
-
-
-class ModelA(BaseModel):
-    id: int
-    name: str
-
-
-class ModelB(BaseModel):
-    title: str
-    description: str
-
-
-class ModelC(BaseModel):
-    id: int
-    name: Optional[str]
-    email: Optional[str]
-
-
-class ModelD(BaseModel):
-    id: int
-    title: Optional[str]
-    department: Optional[str]
 
 
 @patch("modules.webhooks.base.select_best_model")
-def test_validate_payload_no_models(mock_select_best_model):
-    mock_select_best_model.return_value = (ModelA, ModelA(id=1, name="Test"))
-    models = None
+def test_validate_payload_returns_model_if_valid(mock_select_best_model):
     payload = {"id": 1, "name": "Test"}
+    mock_select_best_model.return_value = "model", "validated_payload"
 
-    validated = base.validate_payload(payload, models)
-
-    mock_select_best_model.assert_not_called()
-    assert validated == None
-
-
-@patch("modules.webhooks.base.select_best_model")
-def test_validate_payload_valid_dict(mock_select_best_model):
-    mock_select_best_model.return_value = (ModelA, ModelA(id=1, name="Test"))
-    models = [ModelA, ModelB]
-    payload = {"id": 1, "name": "Test"}
-
-    validated = base.validate_payload(payload, models)
-
-    mock_select_best_model.assert_called_once_with(payload, models, None)
-    assert validated == (ModelA, ModelA(id=1, name="Test"))
-
-
-@patch("modules.webhooks.base.select_best_model")
-def test_validate_payload_valid_json(mock_select_best_model):
-    mock_select_best_model.return_value = (
-        ModelB,
-        ModelB(title="Hello", description="World"),
-    )
-    models = [ModelA, ModelB]
-    payload = '{"title": "Hello", "description": "World"}'
-
-    validated = base.validate_payload(payload, models)
+    validated = base.validate_payload(payload)
 
     mock_select_best_model.assert_called_once_with(
-        {"title": "Hello", "description": "World"}, models, None
+        payload,
+        ANY,
+        None,
     )
-    assert validated == (ModelB, ModelB(title="Hello", description="World"))
+    assert validated == ("model", "validated_payload")
 
 
 @patch("modules.webhooks.base.select_best_model")
-@patch("modules.webhooks.base.json.loads")
-def test_validate_payload_invalid_json(mock_json_loads, mock_select_best_model):
-    models = [ModelA, ModelB]
-    payload = '{"id": 3, "name": "Invalid JSON"}'
-
-    mock_json_loads.side_effect = json.JSONDecodeError("Expecting value", "", 0)
-
-    validated = base.validate_payload(payload, models)
-
-    mock_select_best_model.assert_not_called()
-    assert validated is None
-
-
-@patch("modules.webhooks.base.select_best_model")
-def test_validate_payload_invalid_type(mock_select_best_model):
-    models = [ModelA, ModelB]
-    payload = ["id", 1, "name", "Test"]
-
-    validated = base.validate_payload(payload, models)
-
-    mock_select_best_model.assert_not_called()
+def test_validate_payload_returns_none_if_invalid(mock_select_best_model):
+    payload = {"unknown_field": "value"}
+    mock_select_best_model.return_value = None
+    validated = base.validate_payload(payload)
     assert validated is None
