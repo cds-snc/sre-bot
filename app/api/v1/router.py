d140e7d80686db6d1dc923f4733f67183b19350a
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2025-11-13 22:04:44 +0000

    chore: fmt

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from api.v1.routes.geolocate import router as geolocate_router
 from api.v1.routes.access import router as access_router
 from api.v1.routes.webhooks import router as webhooks_router
+
 # from modules.groups.api import router as groups_router
 
 
