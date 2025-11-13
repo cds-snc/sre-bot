e4ef1010fe02eaaa2f0ade13f02b0754fe571703
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2025-11-13 20:29:19 +0000

    refactor: comment out groups router import in API router

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from api.v1.routes.geolocate import router as geolocate_router
 from api.v1.routes.access import router as access_router
 from api.v1.routes.webhooks import router as webhooks_router
-from modules.groups.api import router as groups_router
+# from modules.groups.api import router as groups_router
 
 
 # Main v1 router (includes all endpoints)
