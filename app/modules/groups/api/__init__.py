d104adf8bfbf3d0828fc76410c9351158bd5913c
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2025-11-28 02:18:13 +0000

    feat: remove unused response formatting functions from groups API initialization

--- app/modules/groups/api/__init__.py
+++ app/modules/groups/api/__init__.py
@@ -1,15 +1,7 @@
 """API layer - HTTP and Slack interfaces."""
 
 from modules.groups.api.controllers import router
-from modules.groups.api.responses import (
-    format_success_response,
-    format_error_response,
-    format_slack_response,
-)
 
 __all__ = [
     "router",
-    "format_success_response",
-    "format_error_response",
-    "format_slack_response",
 ]
