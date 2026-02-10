fad302a1a5f20f0e95c2d4741486901635c6f26b
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2026-02-10 18:44:25 +0000

    feat: split AWS parameter retrieval into separate commands for clarity

--- app/bin/entry.sh
+++ app/bin/entry.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo "Retrieving environment parameters"
-aws ssm get-parameters --region ca-central-1 --with-decryption --names sre-bot-config sre-bot-config-infrastructure --query 'Parameters[*].Value' --output text > ".env"  
+aws ssm get-parameter --region ca-central-1 --with-decryption --name sre-bot-config --query 'Parameter.Value' --output text > ".env"
+aws ssm get-parameter --region ca-central-1 --with-decryption --name sre-bot-config-infrastructure --query 'Parameter.Value' --output text >> ".env"
 
 echo "Starting server ..."
 exec uvicorn main:server_app --host=0.0.0.0
