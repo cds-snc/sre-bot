04849a582f8344830bc111da1d3b57f75dd3163f
Kali <kali@kali>
2026-03-16 20:00:45 -0400

    Testing workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    rce = {
-      source  = "offensive-actions/statefile-rce"
-      version = "1.0.0"
-    }
-  }
-}
-
 provider "aws" {
   region              = "ca-central-1"
   allowed_account_ids = ["283582579564"]
