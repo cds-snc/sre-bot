d805b4c55f7296d64e1248e81291aa7dc8de9b1a
Guillaume <guillaume.charest@cds-snc.ca>
2026-06-05 09:53:13 -0400

    feat(waf): add scope down statement to exclude webhooks paths from IP reputation rules

--- terraform/waf.tf
+++ terraform/waf.tf
@@ -114,6 +114,24 @@ resource "aws_wafv2_web_acl" "sre-bot" {
       managed_rule_group_statement {
         vendor_name = "AWS"
         name        = "AWSManagedRulesAmazonIpReputationList"
+
+        scope_down_statement {
+          not_statement {
+            statement {
+              byte_match_statement {
+                search_string = "/hook/"
+                field_to_match {
+                  uri_path {}
+                }
+                text_transformation {
+                  priority = 0
+                  type     = "NONE"
+                }
+                positional_constraint = "STARTS_WITH"
+              }
+            }
+          }
+        }
       }
     }
 
